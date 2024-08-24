dd5e65a5d47ac50ad89cee61eb26dc72759cddfa
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-08-24 18:11:51 +0530

    Time: 44 ms (63.98%), Space: 16.3 MB (98.35%) - LeetHub

--- 2886-faulty-keyboard/2886-faulty-keyboard.py
+++ 2886-faulty-keyboard/2886-faulty-keyboard.py
@@ -1,14 +1,41 @@
 class Solution:
     def finalString(self, s: str) -> str:
         #count_i=
-        res=""
-        for i in range(len(s)):
-            if s[i]!="i":
-                res+=s[i]
+        # res=""
+        # for i in range(len(s)):
+        #     if s[i]!="i":
+        #         res+=s[i]
+        #     else:
+        #         res=res[::-1]
+
+
+        # return res
+
+
+
+        # counti=0
+        # for i in range(len(s)):
+        #     if s[i]=="i":
+        #         counti+=1
+        # res=""
+        # sp=[]
+        # sp=s.split("i")
+        # places=0
+        # for i in range(len(sp)):
+        #     places+=1
+        #     res+=sp[i][::-1]
+        #     if i<=counti-1:
+        #         break
+                
+        # for i in range(places,len(sp)):
+        #     res+=sp[i]
+            
+        # return res
+
+        stack = []
+        for i in s:
+            if i != 'i':
+                stack.append(i)
             else:
-                res=res[::-1]
-
-
-        return res
-
-
+                stack = stack[::-1]
+        return ''.join(stack)
