007bed540a033abb99ed2c28e269da0f0fcc3a26
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-08-26 16:43:08 +0530

    Time: 46 ms (89.27%), Space: 16.6 MB (33.65%) - LeetHub

--- 3226-minimum-number-game/3226-minimum-number-game.py
+++ 3226-minimum-number-game/3226-minimum-number-game.py
@@ -1,10 +1,18 @@
 class Solution:
     def numberGame(self, nums: List[int]) -> List[int]:
-        nums.sort()
-        l=0
-        r=1
-        while r<len(nums):
-            nums[l],nums[r]=nums[r],nums[l]
-            l+=2
-            r+=2
-        return nums
+        # nums.sort() #O(nlogn)
+        # l=0
+        # r=1
+        # while r<len(nums):
+        #     nums[l],nums[r]=nums[r],nums[l]
+        #     l+=2
+        #     r+=2
+        # return nums
+
+        heapify(nums)
+        arr=[]
+        while nums:
+            a,b=heappop(nums),heappop(nums)
+            arr.append(b)
+            arr.append(a)
+        return arr
