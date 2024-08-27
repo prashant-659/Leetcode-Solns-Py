9290a30399845930032c521303e0d45c95001794
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-08-27 12:51:09 +0530

    Time: 103 ms (90.21%), Space: 17.8 MB (18.02%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -3,11 +3,19 @@ class Solution:
         n=len(nums) 
         #XOR operations of a number with 0 will return the number
         # A^A=0, A^0=A, 1^2=1^2
-        ans=0
-        for i in range(1,n+1):
-            ans^=i
+        # ans=0
+        # for i in range(1,n+1):
+        #     ans^=i
+        # for num in nums:
+        #     ans^=num
+        # return ans
+
+        sum=n*(n+1)/2
+        s_num=0
         for num in nums:
-            ans^=num
-        return ans
+            s_num+=num
+
+        ans=sum-s_num
+        return int(ans)
         
 
