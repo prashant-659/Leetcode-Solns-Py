115d8530490cb845532e37ce3997a1c42d4a6aae
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-08-30 13:07:41 +0530

    Time: 42 ms (11.98%), Space: 16.5 MB (62.82%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -9,11 +9,12 @@ class Solution:
         if not root:
             return None
         
-        tmp=root.left
-        root.left=root.right
-        root.right=tmp
+        
 
         self.invertTree(root.left)
         self.invertTree(root.right)
+        tmp=root.left
+        root.left=root.right
+        root.right=tmp
 
         return root
