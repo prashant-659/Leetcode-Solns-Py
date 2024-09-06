5a8daed7bb124c125ccac8560bc4194ea041091c
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-09-06 16:46:10 +0530

    Time: 90 ms (55.78%), Space: 18.5 MB (30.7%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,23 +1,43 @@
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        preMap=defaultdict(list)
-        visited=set()
-        for u, v in prerequisites:
-            preMap[u].append(v)
+    def canFinish(self, numCourses: int, prereq: List[List[int]]) -> bool:
+        # preMap=defaultdict(list)
+        # visited=set()
+        # for u, v in prerequisites:
+        #     preMap[u].append(v)
             
-        def dfs(curr_course):
-            if curr_course in visited:
+        # def dfs(curr_course):
+        #     if curr_course in visited:
+        #         return False
+        #     if preMap[curr_course]==[]:
+        #         return True
+        #     visited.add(curr_course)
+
+        #     for pre in preMap[curr_course]:
+        #         if not dfs(pre): return False
+
+        #     visited.remove(curr_course)
+        #     preMap[curr_course]=[]
+        #     return True
+        graph={i:[] for i in range(numCourses)}
+        for u, v in prereq:
+            graph[u].append(v)
+
+        visit,cycle=set(),set()
+        res=[]
+        def dfs(crs):
+            if crs in cycle:
                 return False
-            if preMap[curr_course]==[]:
+            if crs in visit:
                 return True
-            visited.add(curr_course)
-
-            for pre in preMap[curr_course]:
-                if not dfs(pre): return False
-
-            visited.remove(curr_course)
-            preMap[curr_course]=[]
+            cycle.add(crs)
+            for child in graph[crs]:
+                if not dfs(child):
+                    return False
+            cycle.remove(crs)
+            visit.add(crs)
             return True
+        
+
 
 
             
