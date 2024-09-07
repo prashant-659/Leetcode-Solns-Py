fbc28ded86629032cdbd750e2cd7cf212d529c69
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-09-07 16:17:42 +0530

    Time: 49 ms (87.63%), Space: 17.1 MB (31.99%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,16 +1,44 @@
 class Solution(object):
     def findRedundantConnection(self, edges):
-        graph = collections.defaultdict(set)
+        # graph = collections.defaultdict(set)
 
-        def dfs(source, target):
-            if source not in seen:
-                seen.add(source)
-                if source == target: return True
-                return any(dfs(nei, target) for nei in graph[source])
+        # def dfs(source, target):
+        #     if source not in seen:
+        #         seen.add(source)
+        #         if source == target: return True
+        #         return any(dfs(nei, target) for nei in graph[source])
 
-        for u, v in edges:
-            seen = set()
-            if u in graph and v in graph and dfs(u, v):
-                return u, v
-            graph[u].add(v)
-            graph[v].add(u)
+        # for u, v in edges:
+        #     seen = set()
+        #     if u in graph and v in graph and dfs(u, v):
+        #         return u, v
+        #     graph[u].add(v)
+        #     graph[v].add(u)
+
+        par=[i for i in range(len(edges)+1) ]
+        rank=[1]*(len(edges)+1)
+
+        def find(n1):
+            p=par[n1]
+
+            while p!=par[p]:
+                p=par[par[p]] #path compression
+                p=par[p]
+            return p
+                
+        def union(n1,n2):
+            p1,p2=find(n1), find(n2)
+            if p1==p2:
+                return False
+            if rank[p1]>rank[p2]:
+                par[p2]=p1
+                rank[p1]+=rank[p2]
+            else:
+                par[p1]=p2
+                rank[p2]+=rank[p1]
+            return True
+        for n1,n2 in edges:
+            if not union(n1,n2):
+                return [n1,n2]
+
+                
