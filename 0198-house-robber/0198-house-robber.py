a5e1ba318482912c3b9501f6852253b13125b757
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-09-08 13:05:38 +0530

    Time: 33 ms (75.65%), Space: 16.4 MB (69.18%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -7,13 +7,19 @@ class Solution:
         #     curr,prev=max(prev+n,curr), curr
         # return curr
         
-        rob1,rob2=0,0
+        # rob1,rob2=0,0
+        # # [rob1,rob2, n, n+1, n+2......] 
+        # for n in nums:
+        #     temp=max(n+rob1, rob2)
+        #     rob1=rob2
+        #     rob2=temp
+        # return rob2
 
-        for n in nums:
-            temp=max(n+rob1, rob2)
-            rob1=rob2
-            rob2=temp
-        return rob2
+        #bottom-up
+        nums.append(0)
+        for i in range(len(nums)-3,-1,-1):
+            nums[i]=max(nums[i]+nums[i+2], nums[i+1])
+        return max(nums[0],nums[1])
         
         
         
