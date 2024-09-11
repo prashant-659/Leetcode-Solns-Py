9ba98a4ad323d0eb219d582f39116edb6bac5a25
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-09-11 08:16:08 +0530

    Time: 33 ms (73.24%), Space: 16.4 MB (93.56%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
@@ -1,12 +1,19 @@
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
+        # res=0
+        # n=start^goal
+        # while n:
+        #     res+=n&1
+        #     n=n>>1
+        # return res
+
+        #Brian kernighan's algo
+        #donot touch zeroes in bits
+        # by subtracting 1 from binary representation, we can get rid of ones
         res=0
         n=start^goal
         while n:
-            res+=n&1
-            n=n>>1
-
-
-
+            n=n&(n-1)
+            res+=1
         return res
         
