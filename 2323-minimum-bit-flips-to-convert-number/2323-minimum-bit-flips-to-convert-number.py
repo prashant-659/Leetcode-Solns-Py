235c00ace863144a2d5e3e2b2be50494124c4af9
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-09-11 08:18:55 +0530

    Time: 35 ms (57.21%), Space: 16.5 MB (26.96%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
@@ -10,10 +10,23 @@ class Solution:
         #Brian kernighan's algo
         #donot touch zeroes in bits
         # by subtracting 1 from binary representation, we can get rid of ones
+        # res=0
+        # n=start^goal
+        # while n:
+        #     n=n&(n-1)
+        #     res+=1
+        # return res
+
+        maxi=max(start, goal)
         res=0
-        n=start^goal
-        while n:
-            n=n&(n-1)
-            res+=1
+        while maxi:
+            if (start&1)!=(goal&1):
+                res+=1
+            start=start>>1
+            goal=goal>>1
+            maxi=maxi>>1
+
         return res
+
+
         
