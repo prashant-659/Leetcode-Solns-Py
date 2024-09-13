3bb546cc1fe610831941379b1b96dddf7fc91fd3
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-09-13 20:23:04 +0530

    Time: 714 ms (20.8%), Space: 24.3 MB (55.92%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.py
@@ -0,0 +1,24 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        if not fruits:
+            return 0
+        if len(fruits)==1:
+            return 1 
+        mp=defaultdict()
+        i,j=0,0
+        res=0
+        while j<len(fruits):
+            mp[fruits[j]]=1+mp.get(fruits[j],0) 
+            while len(mp)>2:
+                mp[fruits[i]]-=1
+                if mp[fruits[i]]==0:
+                    del mp[fruits[i]]
+                i+=1
+                
+            if len(mp)<=2:
+                res=max(res,j-i+1)
+            j+=1
+        return res
+
+                    
+
