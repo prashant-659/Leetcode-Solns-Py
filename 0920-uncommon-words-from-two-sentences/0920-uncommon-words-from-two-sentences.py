e9875ec6a98cda48b991adfbc4da78186c9551d6
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-09-17 08:03:32 +0530

    Time: 30 ms (88.98%), Space: 16.6 MB (5.76%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.py
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.py
@@ -4,10 +4,12 @@ class Solution:
         for w in s1.split(" ") +s2.split(" "):
             count[w]+=1
         
-        res=[]
-        for w, cnt in count.items():
-            if cnt==1:
-                res.append(w)
-        return res
+
+        return [w for w, ct in count.items() if ct ==1]
+        # res=[]
+        # for w, cnt in count.items():
+        #     if cnt==1:
+        #         res.append(w)
+        # return res
         
             
