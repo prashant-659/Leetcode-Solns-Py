698edda34ff2be34342315d3dfcfb3594d52691d
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-09-19 08:42:14 +0530

    Time: 36 ms (69.6%), Space: 16.8 MB (8.08%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -1,25 +1,38 @@
 class Solution:
-    def diffWaysToCompute(self, expression: str) -> List[int]:
-        #O(n*2^n) 
-        operations={
+    operations={
             "+": lambda x,y:x+y,
             "-": lambda x,y:x-y,
             "*": lambda x,y:x*y,
         }
-        def backtrack(left,right):
-            res=[]
-            for i in range(left,right+1):
-                op=expression[i]
-                if op in operations:
-                    nums1=backtrack(left,i-1)
-                    nums2=backtrack(i+1,right)
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        #O(n*2^n) 
+      
+        # def backtrack(left,right):
+        #     res=[]
+        #     for i in range(left,right+1):
+        #         op=expression[i]
+        #         if op in operations:
+        #             nums1=backtrack(left,i-1)
+        #             nums2=backtrack(i+1,right)
 
-                    for n1 in nums1:
-                        for n2 in nums2:
-                            res.append(operations[op](n1,n2))
-            if res == []:
-                res.append(int(expression[left:right+1]))
+        #             for n1 in nums1:
+        #                 for n2 in nums2:
+        #                     res.append(operations[op](n1,n2))
+        #     if res == []:
+        #         res.append(int(expression[left:right+1]))
 
-            return res
-        return backtrack(0,len(expression)-1)
+        #     return res
+        # return backtrack(0,len(expression)-1)
+        res = []
+        for i in range(len(expression)):
+            op = expression[i]
+            if op in self.operations:
+                nums1 = self.diffWaysToCompute(expression[:i])
+                nums2 = self.diffWaysToCompute(expression[i+1:])
 
+                for n1 in nums1:
+                    for n2 in nums2:
+                        res.append(self.operations[op](n1, n2))
+        if res == []:
+            res.append(int(expression))
+        return res
