450af8414e856deaadcba0f162ac9889590b38ed
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-09-20 14:02:14 +0530

    Time: 71 ms (52.09%), Space: 17.1 MB (35.48%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -4,19 +4,62 @@ class Solution:
         # for i in range(len(s)+1):
         #     if s.startswith(r[i:]):
         #         return r[:i] +s
-        def longestSubStringFromStart(s:str):
-            if len(s)==0 or len(s)==1:
-                return s
-            i=len(s)
-            while i>0:
-                if s[:i]==s[:i][::-1]:
+
+
+        # def longestSubStringFromStart(s:str):
+        #     if len(s)==0 or len(s)==1:
+        #         return s
+        #     i=len(s)
+        #     while i>0:
+        #         if s[:i]==s[:i][::-1]:
             
-                    return s[:i],i
-                    break
-                i-=1
-        if len(s)==1 or len(s)==0:
-            return s
-        index = longestSubStringFromStart(s)[1]
-        adds = s[index:][::-1]
-        new = adds +s
-        return new 
+        #             return s[:i],i
+        #             break
+        #         i-=1
+        # if len(s)==1 or len(s)==0:
+        #     return s
+        # index = longestSubStringFromStart(s)[1]
+        # adds = s[index:][::-1]
+        # new = adds +s
+        # return new 
+
+        #TLE - BF
+        # def is_Pali(s,l,r):
+        #     while l<=r:
+        #         if s[l]!=s[r]:
+        #             return False
+        #         l,r=l+1,r-1
+        #     return True
+        # for r in reversed(range(len(s))):
+        #     if is_Pali(s,0,r):
+        #         suffix=s[r+1:]
+                
+        #         return suffix[::-1]+s
+        # return ""
+
+        #rabin karp algo - O(n)
+        # hash functions algo
+        prefix=0
+        suffix=0
+        base=29
+        last_index=0 #1-1
+        power=1
+        mod=10**9 +7
+        for i, c in enumerate(s):
+            char=(ord(c)-ord('a')+1)
+            prefix=(prefix*base)%mod
+            prefix=(prefix+char)%mod
+
+            suffix=(suffix+char*power)%mod
+            power=(power*base) %mod
+            if prefix==suffix:
+                last_index=i
+        suffix=s[last_index+1:]
+        return suffix[::-1]+s
+
+
+
+
+
+
+
