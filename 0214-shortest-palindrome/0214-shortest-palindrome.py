fd75c2c9a5f7195808600f967680ba63156cdf0a
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-09-20 06:53:30 +0530

    Time: 419 ms (28.87%), Space: 17.1 MB (35.48%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -1,7 +1,22 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
-        r=s[::-1]
-        for i in range(len(s)+1):
-            if s.startswith(r[i:]):
-                return r[:i] +s
-        
+        # r=s[::-1]
+        # for i in range(len(s)+1):
+        #     if s.startswith(r[i:]):
+        #         return r[:i] +s
+        def longestSubStringFromStart(s:str):
+            if len(s)==0 or len(s)==1:
+                return s
+            i=len(s)
+            while i>0:
+                if s[:i]==s[:i][::-1]:
+            
+                    return s[:i],i
+                    break
+                i-=1
+        if len(s)==1 or len(s)==0:
+            return s
+        index = longestSubStringFromStart(s)[1]
+        adds = s[index:][::-1]
+        new = adds +s
+        return new 
