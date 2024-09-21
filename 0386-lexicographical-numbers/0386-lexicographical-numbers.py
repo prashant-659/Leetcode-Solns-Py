dce34893a4400b45b6b8589ec7a4aebc69701142
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-09-21 08:28:05 +0530

    Time: 75 ms (60.65%), Space: 22.5 MB (66.63%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -1,17 +1,31 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        res=[]
-        # for i in range(n+1):
-        #     if n==1 or n//10==1:
-        def dfs(cur):
-            if cur>n:
-                return
+        # res=[]
+        # # for i in range(n+1):
+        # #     if n==1 or n//10==1:
+        # def dfs(cur):
+        #     if cur>n:
+        #         return
             
-            res.append(cur)
-            cur*=10
-            for i in range(10):
-                dfs(cur+i)
+        #     res.append(cur)
+        #     cur*=10
+        #     for i in range(10):
+        #         dfs(cur+i)
 
-        for i in range(1,10):
-            dfs(i)
+        # for i in range(1,10):
+        #     dfs(i)
+        # return res
+
+        res=[]
+        cur=1
+        while len(res)<n:
+            res.append(cur)
+
+            if cur*10<=n:
+                cur*=10
+            else:
+                while cur==n or cur%10==9:
+                    cur=cur//10
+                cur+=1
+
         return res
