56c753e5af778c8a3d684a20366c862a4492766a
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-09-22 13:09:06 +0530

    Time: 565 ms (58.69%), Space: 0B (100%) - LeetHub

---
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+with cte as
+(
+    select player_id,event_date,
+    rank() over(partition by player_id order by event_date) as ordered
+    from activity
+)
+select round(sum(case
+        when a2.event_date=date_add(a1.event_date, INTERVAL 1 DAY) THEN 1 ELSE 0 END)/COUNT(DISTINCT a1.player_id)   
+        ,2)  as fraction
+
+from cte a1
+left join cte a2
+on a1.player_id=a2.player_id 
+and a1.ordered=1
+and a2.ordered=2;
