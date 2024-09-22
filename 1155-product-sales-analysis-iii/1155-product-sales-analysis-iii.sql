0244c6398e3f720e40731fff5c4563902298c979
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-09-22 13:56:06 +0530

    Time: 951 ms (77.11%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -1,30 +1,33 @@
 # Write your MySQL query statement below
--- with cte as
--- (
---     select sale_id,product_id, year,quantity, price,
---     row_number() over(partition by product_id order by year asc) as first_year
---     from sales
--- )
--- select p.product_id, c.year as first_year,c.quantity, c.price
--- from cte c
--- inner join product p
--- on c.product_id=p.product_id
--- where c.first_year=1;
-SELECT 
-  product_id, 
-  year AS first_year, 
-  quantity, 
-  price 
-FROM 
-  Sales 
-WHERE 
-  (product_id, year) IN (
-    SELECT 
-      product_id, 
-      MIN(year) AS year 
-    FROM 
-      Sales 
-    GROUP BY 
-      product_id
-  );
+with cte as
+(
+    select sale_id,product_id, year,quantity, price,
+    rank() over(partition by product_id order by year asc) as first_year
+    from sales
+)
+select p.product_id, c.year as first_year,c.quantity, c.price
+from cte c
+inner join product p
+on c.product_id=p.product_id
+where c.first_year=1;
+
+
+
+-- SELECT 
+--   product_id, 
+--   year AS first_year, 
+--   quantity, 
+--   price 
+-- FROM 
+--   Sales 
+-- WHERE 
+--   (product_id, year) IN (
+--     SELECT 
+--       product_id, 
+--       MIN(year) AS year 
+--     FROM 
+--       Sales 
+--     GROUP BY 
+--       product_id
+--   );
 
