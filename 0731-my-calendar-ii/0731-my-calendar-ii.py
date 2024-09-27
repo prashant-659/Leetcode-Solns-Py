cb32f62cdb5aa2078ffa162c5b938172198f57bc
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-09-27 21:17:17 +0530

    Time: 399 ms (68.96%), Space: 17.5 MB (45.71%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.py
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -1,22 +1,38 @@
+
 class MyCalendarTwo:
 
     def __init__(self):
-        self.m=defaultdict()
+        # self.m=defaultdict()
+        self.event=[]
+        self.doublebooking=[]
 
     def book(self, start: int, end: int) -> bool:
         
-        
-        self.m[start]=1+self.m.get(start,0)
-        self.m[end]=self.m.get(end,0)-1
-        sum=0
-        for i in sorted(self.m):
-            sum+=self.m[i]
-
-            if sum>=3:
-                self.m[start]-=1
-                self.m[end]+=1
+        for booking in self.doublebooking:
+            s,e=booking[:]
+            if start<e and end>s:
                 return False
+        
+        for booking in self.event:
+            s,e =booking[:]
+            if start<e and end>s:
+                self.doublebooking.append((max(start,s),min(end,e)))
+       
+
+        self.event.append((start,end))
         return True
+        
+        # self.m[start]=1+self.m.get(start,0)
+        # self.m[end]=self.m.get(end,0)-1
+        # sum=0
+        # for i in sorted(self.m):
+        #     sum+=self.m[i]
+
+        #     if sum>=3:
+        #         self.m[start]-=1
+        #         self.m[end]+=1
+        #         return False
+        # return True
 
 
 # Your MyCalendarTwo object will be instantiated and called as such:
