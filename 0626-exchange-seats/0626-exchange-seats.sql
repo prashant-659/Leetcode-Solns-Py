7d6e593419a01cafb29fca032ce2a1190bc161b9
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-09-27 15:08:54 +0530

    Time: 340 ms (46.24%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,6 +1,14 @@
 # Write your MySQL query statement below
-select id,
-    case when id%2=0 then (lag(student) over (order by id))
-    else ifnull(lead(student) over (order by id), student)
-    end as 'student'
-    from seat
+-- select id,
+--     case when id%2=0 then (lag(student) over (order by id))
+--     else ifnull(lead(student) over (order by id), student)
+--     end as 'student'
+--     from seat
+
+select 
+case 
+when id=(select max(id) from seat) and id%2=1 then id
+when id %2=1 then id+1 else id-1 end as id,   
+student
+from seat
+order by id asc;
