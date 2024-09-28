fdff3b2dfcad941bf4f570388ad17f7798a4c663
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-09-28 08:15:59 +0530

    Time: 60 ms (54.99%), Space: 17.6 MB (7.48%) - LeetHub

--- 0859-design-circular-deque/0859-design-circular-deque.py
+++ 0859-design-circular-deque/0859-design-circular-deque.py
@@ -97,88 +97,185 @@
 # # param_7 = obj.isEmpty()
 # # param_8 = obj.isFull()
 
+# class Node:
+#     def __init__(self, val, nex, prev):
+#         self.val, self.nex, self.prev = val, nex, prev
+
+# class MyCircularDeque:
+
+#     def __init__(self, k: int):
+#         self.size = 0
+#         self.space = k
+#         self.left = Node(0, None, None)
+#         self.right = Node(0, None, self.left)
+#         self.left.nex = self.right
+
+#     def insertFront(self, value: int) -> bool:
+#         if self.isFull():
+#             return False
+        
+#         new_node = Node(value, None, None)
+        
+#         temp = self.left.nex
+    #     temp.prev = new_node
+    #     new_node.nex = temp
+
+    #     self.left.nex = new_node
+    #     new_node.prev = self.left
+
+    #     self.size += 1
+    #     return True
+
+    # def insertLast(self, value: int) -> bool:
+    #     if self.isFull():
+    #         return False
+        
+    #     new_node = Node(value, None, None)
+
+    #     temp = self.right.prev
+    #     temp.nex = new_node
+    #     new_node.prev = temp
+
+    #     new_node.nex = self.right
+    #     self.right.prev = new_node
+
+    #     self.size += 1
+    #     return True
+
+    # def deleteFront(self) -> bool:
+    #     if self.isEmpty():
+    #         return False
+        
+    #     temp = self.left.nex
+    #     self.left.nex = temp.nex
+    #     temp.nex.prev = self.left
+    #     del temp
+
+    #     self.size -= 1
+    #     return True
+
+    # def deleteLast(self) -> bool:
+    #     if self.isEmpty():
+    #         return False
+        
+    #     temp = self.right.prev
+    #     self.right.prev = temp.prev
+    #     temp.prev.nex = self.right
+
+    #     self.size -= 1
+    #     return True
+
+    # def getFront(self) -> int:
+    #     if self.isEmpty():
+    #         return -1
+        
+    #     return self.left.nex.val
+
+    # def getRear(self) -> int:
+    #     if self.isEmpty():
+    #         return -1
+        
+    #     return self.right.prev.val
+
+    # def isEmpty(self) -> bool:
+    #     return self.size == 0
+
+    # def isFull(self) -> bool:
+    #     return self.size == self.space
+
+
 class Node:
-    def __init__(self, val, nex, prev):
-        self.val, self.nex, self.prev = val, nex, prev
+
+    def __init__(self, value: int, prev_node, next_node):
+        self.value = value
+        self.prev_node = prev_node
+        self.next_node = next_node
 
 class MyCircularDeque:
 
     def __init__(self, k: int):
-        self.size = 0
-        self.space = k
-        self.left = Node(0, None, None)
-        self.right = Node(0, None, self.left)
-        self.left.nex = self.right
+        self.max_size = k
+        self.current_size = 0
+        self.front = None
+        self.last = None
 
     def insertFront(self, value: int) -> bool:
         if self.isFull():
             return False
         
-        new_node = Node(value, None, None)
+        if self.isEmpty():
+            new_node = Node(value, None, None)
+            self.front = new_node
+            self.last = new_node
+        else:
+            new_node = Node(value, None, self.front)
+            self.front.prev_node = new_node
+            self.front = new_node
+        self.current_size += 1
         
-        temp = self.left.nex
-        temp.prev = new_node
-        new_node.nex = temp
-
-        self.left.nex = new_node
-        new_node.prev = self.left
-
-        self.size += 1
         return True
 
     def insertLast(self, value: int) -> bool:
         if self.isFull():
             return False
         
-        new_node = Node(value, None, None)
+        if self.isEmpty():
+            new_node = Node(value, None, None)
+            self.front = new_node
+            self.last = new_node
+        else:
+            new_node = Node(value, self.last, None)
+            self.last.next_node = new_node
+            self.last = new_node
+        self.current_size += 1
 
-        temp = self.right.prev
-        temp.nex = new_node
-        new_node.prev = temp
-
-        new_node.nex = self.right
-        self.right.prev = new_node
-
-        self.size += 1
         return True
 
     def deleteFront(self) -> bool:
         if self.isEmpty():
             return False
-        
-        temp = self.left.nex
-        self.left.nex = temp.nex
-        temp.nex.prev = self.left
-        del temp
 
-        self.size -= 1
+        self.current_size -= 1
+        self.front = self.front.next_node
+
         return True
 
     def deleteLast(self) -> bool:
         if self.isEmpty():
             return False
-        
-        temp = self.right.prev
-        self.right.prev = temp.prev
-        temp.prev.nex = self.right
 
-        self.size -= 1
+        self.current_size -= 1
+        self.last = self.last.prev_node
+
         return True
 
     def getFront(self) -> int:
         if self.isEmpty():
             return -1
-        
-        return self.left.nex.val
+
+        return self.front.value
 
     def getRear(self) -> int:
         if self.isEmpty():
             return -1
-        
-        return self.right.prev.val
+
+        return self.last.value
+
 
     def isEmpty(self) -> bool:
-        return self.size == 0
+        return self.current_size == 0
 
     def isFull(self) -> bool:
-        return self.size == self.space
+        return self.current_size == self.max_size
+
+
+# Your MyCircularDeque object will be instantiated and called as such:
+# obj = MyCircularDeque(k)
+# param_1 = obj.insertFront(value)
+# param_2 = obj.insertLast(value)
+# param_3 = obj.deleteFront()
+# param_4 = obj.deleteLast()
+# param_5 = obj.getFront()
+# param_6 = obj.getRear()
+# param_7 = obj.isEmpty()
+# param_8 = obj.isFull()
