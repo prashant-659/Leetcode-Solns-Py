523fecb07abc661f091c64513f7367232f576365
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-09-29 20:32:55 +0530

    Time: 47 ms (69.98%), Space: 16.6 MB (9.71%) - LeetHub

--- 0805-escape-the-ghosts/0805-escape-the-ghosts.py
+++ 0805-escape-the-ghosts/0805-escape-the-ghosts.py
@@ -1,10 +1,12 @@
 class Solution:
     def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
-        d_ghost_to_target=[]
+        d_ghost_to_target=[] 
+        #Manhattan Distance works coz at any point if you are closer to the target, you can reach the target before the ghost does.
+
         for x,y in ghosts:
             d_ghost_to_target.append(abs(x-target[0])+abs(target[1]-y))
         
-        me_to_target=abs(target[0]-0)+abs(target[1]-0)
+        me_to_target=abs(target[0])+abs(target[1])
         for dis in d_ghost_to_target:
             if dis<=me_to_target:
                 return False
