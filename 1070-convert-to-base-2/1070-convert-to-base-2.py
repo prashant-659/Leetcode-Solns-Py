9182051d5ae575b0262bbd5d02c48769f9e54067
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-10-02 11:15:12 +0530

    Time: 30 ms (82.72%), Space: 16.5 MB (37.65%) - LeetHub

---
+++ 1070-convert-to-base-2/1070-convert-to-base-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def baseNeg2(self, n: int) -> str:
+        base, ans=-2, ''
+        while n:
+            n,r=divmod(n,base)
+            if r<0:
+                n,r=n+1,r-base
+            ans=str(r)+ans
+        return max(ans,"0")
