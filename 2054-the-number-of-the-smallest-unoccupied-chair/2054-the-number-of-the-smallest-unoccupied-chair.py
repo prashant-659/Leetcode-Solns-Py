cb85b52aea8963185cdabc0db33ec838b7d26651
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-10-11 08:05:17 +0530

    Time: 559 ms (73.27%), Space: 22.6 MB (93.99%) - LeetHub

---
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.py
@@ -0,0 +1,18 @@
+class Solution:
+    def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
+        times=[(t[0],t[1],i) for i, t in enumerate(times)]
+        times.sort()
+        n=len(times)
+
+        used_chairs=[] #(leavtime, chair)
+        available_chairs=[i for i in range(n)] #chair
+
+        for a,l,i in times:
+            while used_chairs and used_chairs[0][0]<=a:
+                leave,chair=heapq.heappop(used_chairs)
+                heapq.heappush(available_chairs,chair)
+            chair=heapq.heappop(available_chairs)
+            heapq.heappush(used_chairs,(l,chair))
+
+            if i==targetFriend:
+                return chair
