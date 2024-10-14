cfdb0b7d6eb335f6a4525f38213416a9b173804d
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-10-14 13:43:07 +0530

    Time: 1558 ms (97.95%), Space: 61 MB (89.81%) - LeetHub

---
+++ 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.py
@@ -0,0 +1,20 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        trip=[0,0,0]
+        
+        for triplet in triplets:
+            
+            if(triplet[0]>target[0] or triplet[1]>target[1] or triplet[2]>target[2]):
+                pass
+            else:
+                if(triplet[0]==target[0]):
+                    trip[0]+=1
+                if(triplet[1]==target[1]):
+                    trip[1]+=1
+                if(triplet[2]==target[2]):
+                    trip[2]+=1
+        
+        if(trip[0]>0 and trip[1]>0 and trip[2]>0):
+            return True
+        else:
+            return False
