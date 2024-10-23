49abe977d7b3ecfd11a3fb2cab610104ce69635f
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-10-23 15:27:59 +0530

    Time: 0 ms (100%), Space: 16.9 MB (29.15%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,33 +1,32 @@
-# Time: log(min(n, m))
-
-
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        A, B = nums1, nums2
-        total = len(nums1) + len(nums2)
-        half = total // 2
+        A,B=nums1, nums2
+        total=len(nums1)+len(nums2)
+        half=total//2
 
-        if len(B) < len(A):
-            A, B = B, A
+        if len(B)<len(A):
+            A,B=B,A
 
-        l, r = 0, len(A) - 1
+        l,r=0,len(A)-1
         while True:
-            i = (l + r) // 2  # A
-            j = half - i - 2  # B
+            i=(l+r)//2 #A
+            j=half-i-2 #B index=size-2
 
-            Aleft = A[i] if i >= 0 else float("-infinity")
-            Aright = A[i + 1] if (i + 1) < len(A) else float("infinity")
-            Bleft = B[j] if j >= 0 else float("-infinity")
-            Bright = B[j + 1] if (j + 1) < len(B) else float("infinity")
+            Aleft=A[i] if i>=0 else float("-inf")
+            Aright=A[i+1] if (i+1) < len(A) else float("inf")
+            Bleft=B[j] if j>=0 else float("-inf")
+            Bright=B[j+1] if (j+1)< len(B) else float("inf")
 
-            # partition is correct
-            if Aleft <= Bright and Bleft <= Aright:
-                # odd
-                if total % 2:
-                    return min(Aright, Bright)
-                # even
-                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
-            elif Aleft > Bright:
-                r = i - 1
+            if Aleft<=Bright and Bleft<=Aright:
+                if total%2:
+                    return min(Aright,Bright)
+                
+                return (max(Aleft,Bleft)+min(Aright,Bright))/2
+
+            elif Aright>Bright:
+                r=i-1
             else:
-                l = i + 1
+                l=i+1
+            
+
+
