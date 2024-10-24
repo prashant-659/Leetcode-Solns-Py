4b41ed6c95b1f8c267152d22fbfa0fa5ffa203ed
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-10-24 17:19:35 +0530

    Time: 7 ms (94.52%), Space: 16.7 MB (9.53%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,3 +1,19 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return int(pow(x, 0.5))
+        # return int(pow(x, 0.5))
+        if x<=1:
+            return x
+        i=0
+        j=x
+        while i<j:
+            m=(i+j)//2
+            if m*m==x:
+                return m
+            elif m*m<x:
+                i=m+1
+            else:
+                j=m
+        return i-1
+
+
+
