fe714bc105afd444f8d07f638326a325bc909a3c
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-11-03 12:52:09 +0530

    Time: 367 ms (66.48%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -5,10 +5,22 @@
 --     end as 'student'
 --     from seat
 
-select 
-case 
-when id=(select max(id) from seat) and id%2=1 then id
-when id %2=1 then id+1 else id-1 end as id,   
-student
-from seat
-order by id asc;
+-- select 
+-- case 
+-- when id=(select max(id) from seat) and id%2=1 then id
+-- when id %2=1 then id+1 else id-1 end as id,   
+-- student
+-- from seat
+-- order by id asc;
+
+
+
+
+
+select id,
+            case when id%2=0 then lag(student) over(order by id)
+            else ifnull(lead(student) over(order by id), student)
+            end as 'student'
+        from seat;
+
+
