e43fcd67be673b04ca257b2b1fdccb8104435d3f
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-11-06 13:08:41 +0530

    Time: 501 ms (68.47%), Space: 0B (100%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -1,7 +1,10 @@
 # Write your MySQL query statement below
-select id,
-        case when p_id is null then "Root" 
-        when exists (SELECT 1 FROM tree t2 WHERE t2.p_id = t1.id) then "Inner"
-        when not exists (SELECT 1 FROM tree t2 WHERE t2.p_id = t1.id) then "Leaf"
-        end as type
-    from tree t1 ;
+-- select id,
+--         case when p_id is null then "Root" 
+--         when exists (SELECT 1 FROM tree t2 WHERE t2.p_id = t1.id) then "Inner"
+--         when not exists (SELECT 1 FROM tree t2 WHERE t2.p_id = t1.id) then "Leaf"
+--         end as type
+--     from tree t1 ;
+
+select id,case when p_id is null then "Root" when id in (select p_id from tree) then
+"Inner" else "Leaf" end as type from tree
