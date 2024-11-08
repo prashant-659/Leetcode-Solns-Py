469785bd9229dc19eeb20a658a5f9842344985ef
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-11-08 12:34:23 +0530

    Time: 0 ms (100%), Space: 16.6 MB (77.35%) - LeetHub

--- 1095-two-city-scheduling/1095-two-city-scheduling.py
+++ 1095-two-city-scheduling/1095-two-city-scheduling.py
@@ -1,11 +1,17 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        refund=[]
-        mini=0
-        for A, B in costs:
-            refund.append(B-A)
-            mini+=A
-        refund.sort()
-        for i in range(len(costs)//2):
-            mini+=refund[i]
-        return mini
+        # refund=[]
+        # mini=0
+        # for A, B in costs:
+        #     refund.append(B-A)
+        #     mini+=A
+        # refund.sort()
+        # for i in range(len(costs)//2):
+        #     mini+=refund[i]
+        # return mini
+        costs.sort(key = lambda x: x[0]-x[1])
+        total = 0
+        n = len(costs) // 2
+        for i in range(n):
+            total += costs[i][0] + costs[i+n][1]
+        return total
