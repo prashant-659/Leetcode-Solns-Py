2b00701d1dd50153cec4967388e83e5835446d3c
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-11-08 22:51:01 +0530

    Time: 1000 ms (12.1%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -5,14 +5,28 @@
 -- on a.user_id=b.user_id
 -- group by a.user_id;
 
-With cte as 
-(
+-- With cte as 
+-- (
+--     select s.user_id, count(*) as total,
+--             sum(case when action='confirmed' then 1 else 0 end ) as confirmed
+--             from signups s 
+--             left join confirmations c
+--             on s.user_id=c.user_id
+--             group by s.user_id
+--             )
+-- select user_id, round(confirmed/total,2) as confirmation_rate
+-- from cte;
+
+
+with cte as(
     select s.user_id, count(*) as total,
-            sum(case when action='confirmed' then 1 else 0 end ) as confirmed
-            from signups s 
-            left join confirmations c
-            on s.user_id=c.user_id
-            group by s.user_id
-            )
-select user_id, round(confirmed/total,2) as confirmation_rate
-from cte;
+                    sum(case when c.action="confirmed" then 1 else 0 end) as confs
+    from signups s left join confirmations c on s.user_id=c.user_id
+    group by s.user_id
+
+)
+select user_id, round((confs/total),2) as confirmation_rate from cte;
+
+
+
+
