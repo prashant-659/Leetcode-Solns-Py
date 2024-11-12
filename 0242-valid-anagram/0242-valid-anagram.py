f9bdf2b9b9357e911ea1d2e6d7b0f4dbea2ef22b
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-11-12 21:06:48 +0530

    Time: 3 ms (99.1%), Space: 16.9 MB (71.02%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,12 +2,7 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s)!=len(t):
             return False
-        mp=defaultdict(int)
-        for i in range(len(s)):
-            mp[s[i]]+=1
-        for i in range(len(t)):
-            mp[t[i]]-=1
-        for val in mp.values():
-            if val!=0:
-                return False
-        return True
+        counter1=Counter(s)
+        counter2=Counter(t)
+        return counter1==counter2
+            
