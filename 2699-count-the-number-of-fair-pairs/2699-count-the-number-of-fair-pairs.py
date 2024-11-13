ddb5014b0637296477f0620decaaff7d578c75dc
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-11-13 12:03:39 +0530

    Time: 926 ms (28.57%), Space: 30.6 MB (91.73%) - LeetHub

---
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        nums.sort()
+        pairs=0
+        # for i in range(len(nums)):
+        #     for j in range(i):
+        #         if nums[j]+nums[i]>=lower and nums[j]+nums[i]<=upper:
+        #             pairs+=1
+        # return pairs
+
+        def binary_search(l,r,target):
+            while l<=r:
+                m=(l+r)//2
+
+                if nums[m]>=target:
+                    r=m-1
+                else:
+                    l=m+1
+            return r
+
+        r=len(nums)-1
+        for i in range(len(nums)):
+            low=lower-nums[i]
+            up=upper-nums[i]
+            pairs+=(binary_search(i+1,r, up+1)-
+                   binary_search(i+1, r,low) )
+        return pairs
