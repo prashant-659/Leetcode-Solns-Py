dd7d6ca5e2f2ac051bb9565d62f4088501f7ee4d
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-11-14 13:12:30 +0530

    Time: 601 ms (57.92%), Space: 28.7 MB (59.35%) - LeetHub

--- 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.py
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.py
@@ -1,11 +1,22 @@
 class Solution:
     def minimizedMaximum(self, n: int, quantities: List[int]) -> int:
-        l,r=0,max(quantities)
+        def candistribute(x):
+            stores=0
+            for q in quantities:
+                stores+=math.ceil(q/x)
+            return stores<=n
 
-        while l+1<r:
-            m=(l+r)//2
-            if sum(ceil(i/m) for i in quantities)<=n:
-                r=m
+
+        l,r =1, max(quantities)
+
+        res=0
+        while l<=r:
+            x=l+(r-l)//2 
+            if candistribute(x):
+                res=x
+                r=x-1
             else:
-                l=m
-        return r 
+                l=x+1
+        return res
+
+
