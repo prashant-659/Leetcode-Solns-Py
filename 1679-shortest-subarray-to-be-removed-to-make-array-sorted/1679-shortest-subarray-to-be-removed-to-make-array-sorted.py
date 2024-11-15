3fb9dc38746d3d0c03609de8df80a412454fa340
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-11-15 08:45:10 +0530

    Time: 10 ms (90.09%), Space: 30.2 MB (98.13%) - LeetHub

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.py
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -7,18 +7,12 @@ class Solution:
             r-=1
         res=r
 
-        #remove postfix
-        l=0
-        while l+1 <N and arr[l]<=arr[l+1]:
-            l+=1
-        res=min(res,N-1-l)
+        
 
-        #remove  mkddle
-        l,r=0,N-1
+        #remove  middle and postfix
+        l=0
         while l<r:
-            #shrink the valid window
-            while r<N and l+1<r and arr[r-1]<=arr[r] and arr[l]<=arr[r]:
-                r-=1
+            
             
             # expand the invalid window
             while r<N and arr[l]>arr[r]:
