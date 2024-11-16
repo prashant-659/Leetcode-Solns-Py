fb7c85e48021c12a6449b5e8b882994f7d727ec6
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-11-16 08:36:37 +0530

    Time: 143 ms (6.38%), Space: 16.9 MB (21.46%) - LeetHub

--- 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.py
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.py
@@ -15,6 +15,67 @@
         #         res.append(-1)
         # return res
 
+# class Solution:
+#     def resultsArray(self, nums: List[int], k: int) -> List[int]:
+#         i,j=0,0
+#         max_ele=0
+#         res=[]
+#         while j<len(nums):
+#             if nums[j]>max_ele:
+#                 max_ele=nums[j]
+
+#             if (j-i+1)<k:
+#                 j+=1
+#             elif (j-i+1)==k:
+#                 res.append(max(max_ele, nums[i]))
+            
+#             i+=1
+#             j+=1
+#         return res
+
+
+
+
+
+
+
+
+        # n = len(nums)
+        # result = []
+        # for i in range(n - k + 1):
+        #     is_valid = True
+        #     max_element = nums[i]
+        #     for j in range(i, i + k - 1):
+        #         if nums[j] + 1 != nums[j + 1]:
+        #             is_valid = False
+        #             break
+        #         max_element = max(max_element, nums[j])
+            
+        #     if is_valid:
+        #         result.append(max(max_element, nums[i + k - 1]))
+        #     else:
+        #         result.append(-1)
+        
+        # return result       
+                
+
+# class Solution:
+#     def resultsArray(self, nums: List[int], k: int) -> List[int]:
+        # res=[]
+        # for i in range(len(nums)-k+1):
+        #     is_valid=True
+        #     max_el=nums[i]
+        #     for j in range(i,i+k-1):
+        #         if nums[j]+1 != nums[j+1]:
+        #             isvalid=False
+        #             break
+        #         max_ele=max(max_el,nums[j])
+        #     if is_valid:
+        #         res.append(max(max_el,nums[i-k+1]))
+        #     else:
+        #         res.append(-1)
+        # return res
+
 class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
         n = len(nums)
@@ -34,6 +95,3 @@ class Solution:
                 result.append(-1)
         
         return result       
-                
-
-
