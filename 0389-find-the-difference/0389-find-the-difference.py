5d5f18d1bec2531d86e6d5d2161803187dc1402a
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-11-18 20:04:34 +0530

    Time: 0 ms (100%), Space: 16.7 MB (10.55%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,12 +1,16 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         c=0
-        for cs in s:
-            c^=ord(cs)
-        for ct in t:
-            c^=ord(ct)
+        # for cs in s:
+        #     c^=ord(cs)
+        # for ct in t:
+        #     c^=ord(ct)
+        # return chr(c)
+        for t1 in t:
+            c+=ord(t1)
+        for s1 in s:
+            c-=ord(s1)
         return chr(c)
-
 
             
         
