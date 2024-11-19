8cd8eaa32f49b4913590a478a67f27e4c6bde1e8
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-11-20 00:10:09 +0530

    Time: 103 ms (69.39%), Space: 19.9 MB (66.27%) - LeetHub

--- 2954-maximum-sum-of-almost-unique-subarray/2954-maximum-sum-of-almost-unique-subarray.py
+++ 2954-maximum-sum-of-almost-unique-subarray/2954-maximum-sum-of-almost-unique-subarray.py
@@ -1,51 +1,52 @@
-# class Solution:
-#     def maxSum(self, nums: List[int], m: int, k: int) -> int:
-#         i=0
-#         j=0
-#         res=0
-#         sub=[]
-#         s={}
-#         maxi=0
-#         while j<len(nums):
-#             res+=nums[j]
-#             s[nums[j]]=s.get(nums[j],0)+1
-            
-#             if j-i+1==k:
-#                 if len(s)>=m:
-#                     maxi=max(res,maxi)
-#             if j - i + 1 > k:
-#                 res -= nums[i]
-#                 s[nums[i]]-=1
-#                 if s[nums[i]]==0:
-#                     del s[nums[i]]
-#                 i += 1 
-#             j+=1
-#         return maxi
-
-                
 class Solution:
     def maxSum(self, nums: List[int], m: int, k: int) -> int:
-        i = 0
-        res = 0
-        s = {}
-        maxi = 0
-
-        for j in range(len(nums)):
-            # Add nums[j] to the window
-            res += nums[j]
-            s[nums[j]] = s.get(nums[j], 0) + 1
-
-            # Shrink the window if its size exceeds k
+        i=0
+        j=0
+        res=0
+        s={}
+        maxi=0
+        while j<len(nums):
+            res+=nums[j]
+            s[nums[j]]=s.get(nums[j],0)+1
+            
             if j - i + 1 > k:
                 res -= nums[i]
-                s[nums[i]] -= 1
-                if s[nums[i]] == 0:  # Remove from map if count becomes 0
+                s[nums[i]]-=1
+                if s[nums[i]]==0:
                     del s[nums[i]]
-                i += 1
+                i += 1 
 
-            # When the window size is exactly k, check the "almost unique" condition
-            if j - i + 1 == k:
-                if len(s) >= m:  # At least `m` distinct elements
-                    maxi = max(maxi, res)
-
+            if j-i+1==k:
+                if len(s)>=m:
+                    maxi=max(res,maxi)
+            
+            j+=1
         return maxi
+
+                
+# class Solution:
+#     def maxSum(self, nums: List[int], m: int, k: int) -> int:
+#         i = 0
+#         res = 0
+#         s = {}
+#         maxi = 0
+
+#         for j in range(len(nums)):
+#             # Add nums[j] to the window
+#             res += nums[j]
+#             s[nums[j]] = s.get(nums[j], 0) + 1
+
+#             # Shrink the window if its size exceeds k
+#             if j - i + 1 > k:
+#                 res -= nums[i]
+#                 s[nums[i]] -= 1
+#                 if s[nums[i]] == 0:  # Remove from map if count becomes 0
+#                     del s[nums[i]]
+#                 i += 1
+
+#             # When the window size is exactly k, check the "almost unique" condition
+#             if j - i + 1 == k:
+#                 if len(s) >= m:  # At least `m` distinct elements
+#                     maxi = max(maxi, res)
+
+#         return maxi
