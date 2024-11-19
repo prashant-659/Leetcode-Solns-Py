e95977ba9468c1766700c9ee8f9aa4a2c88cff0c
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-11-20 00:19:18 +0530

    Time: 118 ms (56.78%), Space: 36.3 MB (34.3%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -7,35 +7,53 @@ class Solution:
         #         mp[i]=nums[i]
         #     else:
         #         mp[i]=1+mp[i]
-        i = 0
-        j = 0
-        curr_sum = 0
-        ans = 0
-        freq_map = defaultdict(int)
+        # i = 0
+        # j = 0
+        # curr_sum = 0
+        # ans = 0
+        # freq_map = defaultdict(int)
         
-        while j < len(nums):
-            curr_sum += nums[j]
-            freq_map[nums[j]] += 1
+        # while j < len(nums):
+        #     curr_sum += nums[j]
+        #     freq_map[nums[j]] += 1
             
-            if j - i + 1 < k:
-                j += 1
-            elif j - i + 1 == k:
-                if len(freq_map) == k:
-                    ans = max(ans, curr_sum)
-                    print(ans, end=" ")
+        #     if j - i + 1 < k:
+        #         j += 1
+        #     elif j - i + 1 == k:
+        #         if len(freq_map) == k:
+        #             ans = max(ans, curr_sum)
+        #             print(ans, end=" ")
                 
-                if nums[i] in freq_map:
-                    freq_map[nums[i]] -= 1
-                    if freq_map[nums[i]] == 0:
-                        del freq_map[nums[i]]
+        #         if nums[i] in freq_map:
+        #             freq_map[nums[i]] -= 1
+        #             if freq_map[nums[i]] == 0:
+        #                 del freq_map[nums[i]]
                 
-                curr_sum -= nums[i]
-                i += 1
-                j += 1
+        #         curr_sum -= nums[i]
+        #         i += 1
+        #         j += 1
         
-        return ans
+        # return ans
 
-
+        res=0
+        maxi=0
+        i=0
+        mp={}
+        mp={}
+        for j in range(len(nums)):
+            res+=nums[j]
+            mp[nums[j]]=1+mp.get(nums[j],0)
+            if j-i+1==k:
+                if len(mp)==k:
+                    maxi=max(res,maxi)
+                
+                if nums[i] in mp:
+                    mp[nums[i]]-=1
+                    if mp[nums[i]]==0:
+                        del mp[nums[i]]
+                res-=nums[i]
+                i+=1
+        return maxi
 
  
                
