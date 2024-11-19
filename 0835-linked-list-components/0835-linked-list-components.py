7a1d897d7942373f0afb9bc80509102272a579e1
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-11-19 19:57:11 +0530

    Time: 8 ms (30.83%), Space: 21 MB (27.86%) - LeetHub

---
+++ 0835-linked-list-components/0835-linked-list-components.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def numComponents(self, head: Optional[ListNode], nums: List[int]) -> int:
+        mp=set(nums)
+        node=head
+        res=0
+        flag=False
+        while node: 
+            if node.val not in mp and flag:
+                res+=1
+                flag=False
+            elif node.val in mp:
+                flag=True
+            node=node.next
+            
+            
+                 
+        if flag:
+            res+=1
+        return res
+        
+    
+    
+    
+
+        
