cc11b94497fb8a3129f03219f8bc15433df84d58
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-11-19 20:01:27 +0530

    Time: 3 ms (80.24%), Space: 21 MB (9.06%) - LeetHub

--- 0835-linked-list-components/0835-linked-list-components.py
+++ 0835-linked-list-components/0835-linked-list-components.py
@@ -5,23 +5,32 @@
 #         self.next = next
 class Solution:
     def numComponents(self, head: Optional[ListNode], nums: List[int]) -> int:
-        mp=set(nums)
-        node=head
-        res=0
-        flag=False
-        while node: 
-            if node.val not in mp and flag:
-                res+=1
-                flag=False
-            elif node.val in mp:
-                flag=True
-            node=node.next
+        # mp=set(nums)
+        # node=head
+        # res=0
+        # flag=False
+        # while node: 
+        #     if node.val not in mp and flag:
+        #         res+=1
+        #         flag=False
+        #     elif node.val in mp:
+        #         flag=True
+        #     node=node.next
             
             
                  
-        if flag:
-            res+=1
-        return res
+        # if flag:
+        #     res+=1
+        # return res
+
+        s=set(nums)
+        ans=0
+        node=head
+        while node:
+            if node.val in s and not(node.next and node.next.val in s):
+                ans+=1
+            node=node.next
+        return ans
         
     
     
