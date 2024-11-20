5ce7dfe6fbc510842d3fa9ae48232400e276158d
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-11-20 14:24:18 +0530

    Time: 119 ms (98.89%), Space: 17.1 MB (79.81%) - LeetHub

--- 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.py
+++ 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.py
@@ -1,24 +1,38 @@
 class Solution:
     def takeCharacters(self, s: str, k: int) -> int:
         
-        ct=[0,0,0]
-        for c in s:
-            ct[ord(c)-ord("a")]+=1
-        if min(ct)<k:
-            return -1
+        # ct=[0,0,0]
+        # for c in s:
+        #     ct[ord(c)-ord("a")]+=1
+        # if min(ct)<k:
+        #     return -1
         
-        res=float("inf")
+        # res=float("inf")
 
-        l=0 
-        for r in range(len(s)):
-            ct[ord(s[r])-ord("a")]-=1
-            while min(ct)<k:
-                ct[ord(s[l])-ord("a")]+=1
+        # l=0 
+        # for r in range(len(s)):
+        #     ct[ord(s[r])-ord("a")]-=1
+        #     while min(ct)<k:
+        #         ct[ord(s[l])-ord("a")]+=1
+        #         l+=1
+        #     res=min(res,len(s)-(r-l+1))
+        # return res
+
+        freq=[0]*3
+        size=len(s)
+        for c in s:
+            freq[ord(c)-ord("a")]+=1
+        
+        l,r=0,0
+        if freq[0]<k or freq[1]<k or freq[2]<k:
+            return -1
+        for r in range(size):
+            freq[ord(s[r])-ord("a")]-=1
+            if freq[0]<k or  freq[1]<k or  freq[2]<k:
+                freq[ord(s[l])-ord("a")]+=1
                 l+=1
-            res=min(res,len(s)-(r-l+1))
-        return res
+        return size-(r-l+1)
 
-                
 
 
 
