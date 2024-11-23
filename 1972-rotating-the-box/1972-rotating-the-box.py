2c827678c5cf25a0e6f5ed570e2c24ec980d94c6
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-11-23 12:40:21 +0530

    Time: 1876 ms (77.21%), Space: 29 MB (65.69%) - LeetHub

--- 1972-rotating-the-box/1972-rotating-the-box.py
+++ 1972-rotating-the-box/1972-rotating-the-box.py
@@ -1,19 +1,20 @@
 class Solution:
     def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
-        orient=[["."]* len(box) for _ in range(len(box[0]))]
+        res=[["."]* len(box) for _ in range(len(box[0]))]
         for r in range(len(box)):
-            i=len(box[0])-1
+            i=len(box[0])-1 #cols-1
             for c in range(len(box[0])-1,-1,-1):
                 if box[r][c]=="#":
-                    box[r][c], box[r][i]=box[r][i], box[r][c]
+                    res[i][len(box)-r-1]="#"
                     i-=1
                 elif box[r][c]=="*":
+                    res[c][len(box)-r-1]="*"
                     i=c-1
-        res=[]
-        for c in range(len(box[0])):
-            col=[]
-            for r in reversed(range(len(box))):
-                col.append(box[r][c])
+        # res=[]
+        # for c in range(len(box[0])):
+        #     col=[]
+        #     for r in reversed(range(len(box))):
+        #         col.append(box[r][c])
             
-            res.append(col)
+        #     res.append(col)
         return res
