0be4cea058d02bfc61aa1a367c0a8dd42e9e1030
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-11-24 08:55:18 +0530

    Time: 85 ms (63.91%), Space: 24.9 MB (72.1%) - LeetHub

--- 2089-maximum-matrix-sum/2089-maximum-matrix-sum.py
+++ 2089-maximum-matrix-sum/2089-maximum-matrix-sum.py
@@ -1,16 +1,32 @@
 class Solution:
     def maxMatrixSum(self, matrix: List[List[int]]) -> int:
-        s,c,z,m=0,0,0,float('inf')
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j]<0:
-                    c+=1
-                if matrix[i][j]==0:
-                    z=1
-                s+=abs(matrix[i][j])
-                m=min(m,abs(matrix[i][j]))
+        # s,c,z,m=0,0,0,float('inf')
+        # for i in range(len(matrix)):
+        #     for j in range(len(matrix[0])):
+        #         if matrix[i][j]<0:
+        #             c+=1
+        #         if matrix[i][j]==0:
+        #             z=1
+        #         s+=abs(matrix[i][j])
+        #         m=min(m,abs(matrix[i][j]))
                 
-        if c%2==0 or z>0:
-            return s
-        else:
-            return s-m*2
+        # if c%2==0 or z>0:
+        #     return s
+        # else:
+        #     return s-m*2
+        res=0
+
+        neg_ct=0
+        mat_min=inf
+        for row in matrix:
+            for n in row:
+                res+=abs(n)
+                mat_min=min(mat_min, abs(n))
+                if n<0:
+                    neg_ct+=1
+
+
+        if neg_ct&1:
+            res-=2*mat_min
+
+        return res
