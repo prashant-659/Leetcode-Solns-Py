4c0adc3d9f030c3991b8852e3478e35f2feae8f1
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-11-26 21:33:22 +0530

    Time: 59 ms (35.24%), Space: 17.4 MB (16.73%) - LeetHub

---
+++ 3516-design-neighbor-sum-service/3516-design-neighbor-sum-service.py
@@ -0,0 +1,37 @@
+
+class NeighborSum:
+
+    def __init__(self, grid: List[List[int]]):
+        self.grid=grid
+        self.l=len(grid)
+        self.position = {grid[i][j]: (i, j) for i in range(self.l)  for j in range(self.l)}
+
+    def adjacentSum(self, value: int) -> int:
+        i, j = self.position[value]
+        adjacent = [
+            (i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)
+        ]
+        sum = 0
+        for a, b in adjacent:
+            if 0 <= a < self.l and 0 <= b < self.l:
+                sum += self.grid[a][b]
+        return sum
+
+    def diagonalSum(self, value: int) -> int:
+        if value not in self.position:
+            return 0
+        i, j = self.position[value]
+        diagonal= [
+            (i - 1, j - 1), (i - 1, j + 1), (i + 1, j - 1), (i + 1, j + 1)
+        ]
+        sum = 0
+        for x, y in diagonal:
+            if 0 <= x < self.l and 0 <= y < self.l:
+                sum += self.grid[x][y]
+        return sum
+
+
+# Your neighborSum object will be instantiated and called as such:
+# obj = neighborSum(grid)
+# param_1 = obj.adjacentSum(value)
+# param_2 = obj.diagonalSum(value)
