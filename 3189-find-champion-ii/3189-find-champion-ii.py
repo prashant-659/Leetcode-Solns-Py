7fb323e685262432913054afbb8033ecf1cb6bfc
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-11-26 13:24:57 +0530

    Time: 16 ms (28.48%), Space: 17.8 MB (58.3%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.py
+++ 3189-find-champion-ii/3189-find-champion-ii.py
@@ -1,32 +1,29 @@
 class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
-        # graph=defaultdict(list)
-        # for u, v in graph:
-        #     graph[u].append(v)
-        # allin=graph.keys()
-        # val=-1
-        # multiple=set()
-        # for j in allin:
-        #     for i in graph.values():
-        #         if j in i:
-        #             continue
-        #         else:
-        #             val=j
-        #             multiple.add(val)
-        
-        
-        # return val
-        if not edges:
-            return 0 if n==1 else -1
-        s=set()
+        incoming=[0]*n
+        for u, v in edges:
+            incoming[v]+=1
 
-        for e in edges:
-            s.add(e[1])
-        if len(s)<n-1:
+        champions=[]
+        for i, incoming_cnt in enumerate(incoming):
+                if not incoming_cnt:     
+                    champions.append(i)
+        
+        
+        if len(champions)>1:
             return -1
-        for i in range(n):
-            if i not in s:
-                return i
+        return champions[0]
+        # if not edges:
+        #     return 0 if n==1 else -1
+        # s=set()
+
+        # for e in edges:
+        #     s.add(e[1])
+        # if len(s)<n-1:
+        #     return -1
+        # for i in range(n):
+        #     if i not in s:
+        #         return i
             
 
 
