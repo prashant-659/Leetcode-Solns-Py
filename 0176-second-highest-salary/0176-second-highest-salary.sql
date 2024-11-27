13b276a7be729a395c0a46bb9c6703255717ea08
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-11-27 19:10:53 +0530

    Time: 322 ms (35.71%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -6,7 +6,9 @@
 --   FROM CTE
 --  WHERE RANK_desc = 2
 
-select (
-    select distinct salary from employee order by salary desc limit 1 offset 1
-)
-as SecondHighestSalary;
+-- select (
+--     select distinct salary from employee order by salary desc limit 1 offset 1
+-- )
+-- as SecondHighestSalary;
+select max(salary) as SecondHighestSalary from employee where salary <(select max(salary) from employee);
+
