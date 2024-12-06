7d303ec667547f8520809ecc3caeb862c137af0c
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-12-06 20:40:45 +0530

    Time: 24 ms (100%), Space: 26.2 MB (49.18%) - LeetHub

--- 3080-split-array-into-maximum-number-of-subarrays/3080-split-array-into-maximum-number-of-subarrays.py
+++ 3080-split-array-into-maximum-number-of-subarrays/3080-split-array-into-maximum-number-of-subarrays.py
@@ -1,15 +1,25 @@
+# class Solution:
+#     def maxSubarrays(self, nums: List[int]) -> int:
+#         min_and = reduce(and_, nums)
+        
+#         if min_and > 0 : return 1
+        
+#         count, mask = 0, 2**32-1
+
+#         for n in nums:
+#             mask &= n
+#             if mask == 0:
+#                 count += 1
+#                 mask = 2**32-1
+
+#         return count
+
 class Solution:
     def maxSubarrays(self, nums: List[int]) -> int:
-        min_and = reduce(and_, nums)
-        
-        if min_and > 0 : return 1
-        
-        count, mask = 0, 2**32-1
-
-        for n in nums:
-            mask &= n
-            if mask == 0:
-                count += 1
-                mask = 2**32-1
-
-        return count
+        score, ans = -1, 1
+        for num in nums:
+            score &= num
+            if score == 0:
+                score = -1
+                ans += 1
+        return 1 if ans == 1 else ans - 1
