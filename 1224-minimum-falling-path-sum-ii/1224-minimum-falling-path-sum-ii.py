d544fe75c54630b5e89b5d488cc288214a6ace27
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-12-20 14:25:25 +0530

    Time: 86 ms (61.52%), Space: 20.7 MB (35.28%) - LeetHub

--- 1224-minimum-falling-path-sum-ii/1224-minimum-falling-path-sum-ii.py
+++ 1224-minimum-falling-path-sum-ii/1224-minimum-falling-path-sum-ii.py
@@ -22,16 +22,82 @@ class Solution:
         #     res=min(res, helper(0,c))
         # return res
 
-        dp=grid[0]
-        for r in range(1,N):
-            next_dp=[float("inf")]*N
+        # dp=grid[0]
+        # for r in range(1,N):
+        #     next_dp=[float("inf")]*N
 
+        #     for curr_c in range(N):
+        #         for prev_c in range(N):
+        #             if prev_c!=curr_c:
+        #                 next_dp[curr_c]=min(
+        #                     next_dp[curr_c], 
+        #                     grid[r][curr_c]+dp[prev_c]
+        #                 )
+        # #     dp=next_dp
+        # # return min(dp) #O(n^3)
+        # res=float("inf")
+        
+        # def getMintwo(row):
+        #     two_smallest=[]#(val, idx)
+
+
+        #     for val, idx in row:
+        #         if len(two_smallest)<2:
+        #             two_smallest.append((val,idx))
+        #         elif two_smallest[1][0]>val:
+        #             two_smallest.pop()
+        #             two_smallest.append((val, idx))
+        #         two_smallest.sort()
+        #     return two_smallest
+
+        #     N= len(grid)
+        #     first_row=[(val, idx) for idx, val in enumerate(grid[0])]
+
+        #     dp=getMintwo(first_row)
+
+        #     for r in range(1,   N):
+        #         next_dp=[] #(val, idx)
+        #         for curr_c in range(N):
+        #             curr_val=grid[r][curr_c]
+        #             min_val=float("inf")
+
+        #             for prev_val, prev_c in dp:
+        #                 if curr_c!=prev_c:
+        #                     min_val=min(min_val, curr_val+prev_val) 
+        #             next_dp.append((min_val, curr_c))
+        #             next_dp=getMintwo(next_dp)
+        #         dp=next_dp
+
+        #     return min([val for val, idx in dp])
+class Solution:
+    def minFallingPathSum(self, grid: list[list[int]]) -> int:
+        res = float("inf")
+        
+        def getMintwo(row):
+            two_smallest = []
+            for val, idx in row:
+                if len(two_smallest) < 2:
+                    two_smallest.append((val, idx))
+                elif two_smallest[1][0] > val:
+                    two_smallest.pop()
+                    two_smallest.append((val, idx))
+                two_smallest.sort()
+            return two_smallest
+        
+        N = len(grid)
+        first_row = [(val, idx) for idx, val in enumerate(grid[0])]
+        dp = getMintwo(first_row)
+        
+        for r in range(1, N):
+            next_dp = []
             for curr_c in range(N):
-                for prev_c in range(N):
-                    if prev_c!=curr_c:
-                        next_dp[curr_c]=min(
-                            next_dp[curr_c], 
-                            grid[r][curr_c]+dp[prev_c]
-                        )
-            dp=next_dp
-        return min(dp)
+                curr_val = grid[r][curr_c]
+                min_val = float("inf")
+                for prev_val, prev_c in dp:
+                    if curr_c != prev_c:
+                        min_val = min(min_val, curr_val + prev_val) 
+                next_dp.append((min_val, curr_c))
+            next_dp = getMintwo(next_dp)
+            dp = next_dp
+        
+        return min([val for val, idx in dp])
