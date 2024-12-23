8f428f9e40f533b42d6b954a6a64bfabd7895508
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-12-23 13:03:04 +0530

    Time: 0 ms (100%), Space: 19.3 MB (10.63%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.py
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.py
@@ -4,15 +4,36 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+    # def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        # node=root
+        # while node:
+        #     if node.val==val:
+        #         return node
+        #     if node.val<val:
+        #         node=node.right
+        #     else:
+        #         node=node.left
+        # return None
+        # if not root:
+        #     return None
+        # if root.val==val:
+        #     return root
+        # elif root.val>val:
+        #     self.searchBST(root.left, val)
+        # else:
+        #     self.searchBST(root.right, val)
 class Solution:
-    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        node=root
-        while node:
-            if node.val==val:
-                return node
-            if node.val<val:
-                node=node.right
-            else:
-                node=node.left
-        return None
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return None
+        if root.val == val:
+            return root
+        elif root.val > val:
+            return self.searchBST(root.left, val)           
+        else:
+            return self.searchBST(root.right, val)
+        
+       
+
                 
