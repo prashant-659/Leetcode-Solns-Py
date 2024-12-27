c330b4baf5d4ef6e711b1f132e2f928f8f975cbc
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-12-27 12:13:24 +0530

    Time: 3 ms (53.06%), Space: 17.9 MB (5.98%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -3,7 +3,18 @@ class Solution:
         # dp=[[0]*(n//2+1) for _ in range(n//2+1)]
 
         # return dp[0]
+        #O(n^2) and O(n) space
         dp={1:1}
+
+        for num in range(2, n+1):
+            dp[num]=0 if num==n else num
+            for i in range(1,num//2+1):
+                val=dp[i]*dp[num-i]
+                dp[num]=max(dp[num], val)
+        return dp[n]
+
+
+
         def dfs(num):
             if num in dp:
                 return dp[num]
