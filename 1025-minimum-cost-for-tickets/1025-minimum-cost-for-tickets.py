39da9170b4266c8a5dea4335f9a8758ee13510a3
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-12-31 16:57:36 +0530

    Time: 3 ms (80.42%), Space: 17.7 MB (33.52%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.py
@@ -1,17 +1,29 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        dp={}
-        def dfs(i):
-            if i==len(days):
-                return 0
-            if i in dp:
-                return dp[i]
-            dp[i]=float("inf")
-            j=i
-            for d, c in zip([1,7,30], costs):
+        # dp={}
+        # def dfs(i):
+        #     if i==len(days):
+        #         return 0
+        #     if i in dp:
+        #         return dp[i]
+        #     dp[i]=float("inf")
+        #     j=i
+        #     for d, c in zip([1,7,30], costs):
                 
-                while j<len(days) and days[j]<days[i]+d:
+        #         while j<len(days) and days[j]<days[i]+d:
+        #             j+=1
+        #         dp[i]=min(dp[i],c+dfs(j))
+        #     return dp[i]
+        # return dfs(0)
+
+        dp=[0]*(len(days)+1)
+
+        for i in reversed(range(len(days))):
+            j=i
+            dp[i]=float("inf")
+
+            for cost, duration in zip(costs,[1,7,30]):
+                while j<len(days) and days[j]<days[i]+duration:
                     j+=1
-                dp[i]=min(dp[i],c+dfs(j))
-            return dp[i]
-        return dfs(0)
+                dp[i]=min(dp[i], cost+dp[j])
+        return dp[0]
