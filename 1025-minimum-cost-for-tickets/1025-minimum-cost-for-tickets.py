0c683cdcc2cca830768194cee362099459137eef
Prashant <97583940+prashant-659@users.noreply.github.com>
2024-12-31 16:49:34 +0530

    Time: 7 ms (44.54%), Space: 18 MB (13.07%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.py
@@ -7,9 +7,9 @@ class Solution:
             if i in dp:
                 return dp[i]
             dp[i]=float("inf")
-
+            j=i
             for d, c in zip([1,7,30], costs):
-                j=i
+                
                 while j<len(days) and days[j]<days[i]+d:
                     j+=1
                 dp[i]=min(dp[i],c+dfs(j))
