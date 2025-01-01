1ac82a6b534304ca0ca1c54aabceb165f8d1dbfd
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-01-01 13:30:14 +0530

    Time: 25 ms (89.48%), Space: 20.4 MB (20.19%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -6,25 +6,34 @@ class Solution:
         # d = dict()
         # d[0] = 1
         
-        # for i in range(len(nums)):
-        #     sums += nums[i]
-        #     count += d.get(sums-k,0)
-        #     d[sums] = d.get(sums,0) + 1
+        # # for i in range(len(nums)):
+        # #     sums += nums[i]
+        # #     count += d.get(sums-k,0)
+        # #     d[sums] = d.get(sums,0) + 1
         
-        # return(count)
+        # # return(count)
 
-        count={}
-        csum=0
-        res=0
-        for n in nums:
-            csum+=n
-            if csum==k:
-                res+=1
-            if csum-k in count:
-                res+=count[csum-k]
+        # count={}
+        # csum=0
+        # res=0
+        # for n in nums:
+        #     csum+=n
+        #     if csum==k:
+        #         res+=1
+        #     if csum-k in count:
+        #         res+=count[csum-k]
             
-            if csum in count:
-                count[csum]+=1
-            else:
-                count[csum]=1
+        #     if csum in count:
+        #         count[csum]+=1
+        #     else:
+        #         count[csum]=1
+        # return res
+
+        prefix={0:1}
+        res=curSum=0
+        for i in range(len(nums)):
+            curSum+=nums[i]
+            diff=curSum-k
+            res+=prefix.get(diff,0)
+            prefix[curSum]=1+prefix.get(curSum,0)
         return res
