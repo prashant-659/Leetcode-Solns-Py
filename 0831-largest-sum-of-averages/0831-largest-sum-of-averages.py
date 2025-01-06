8d240f3b90efa4bd876348ca084f7ea2adefcab4
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-01-06 14:00:58 +0530

    Time: 217 ms (41.05%), Space: 19.4 MB (8.8%) - LeetHub

---
+++ 0831-largest-sum-of-averages/0831-largest-sum-of-averages.py
@@ -0,0 +1,27 @@
+class Solution:
+    def largestSumOfAverages(self, nums: List[int], k: int) -> float:
+        N=len(nums)
+        dp={}
+        
+        def largestSuffix(index, kleft):
+            if index==N:    
+                if kleft==0:
+                    return 0
+                return float("-inf")
+            if kleft==0:
+                return float("-inf")
+            if (index, kleft) in dp:
+                return dp[(index,kleft)]
+            count=0
+            curSum=0
+            best=0.
+            for i in range(index, N):
+                curSum+=nums[i]
+                count+=1
+                best=max(best, curSum/count+largestSuffix(i+1, kleft-1))
+            dp[(index, kleft)]=best
+            return dp[(index,kleft)]
+        
+        return largestSuffix(0,k)
+
+        
