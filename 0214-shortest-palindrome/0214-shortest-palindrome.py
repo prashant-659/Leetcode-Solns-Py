fbe062b5b22e9eee7e551dabd3de905dcadf285e
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-01-09 22:28:44 +0530

    Time: 44 ms (84.54%), Space: 18.4 MB (44.19%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -42,24 +42,22 @@ class Solution:
         prefix=0
         suffix=0
         base=29
-        last_index=0 #1-1
+        last_index=0 #-1
         power=1
-        mod=10**9 +7
-        for i, c in enumerate(s):
-            char=(ord(c)-ord('a')+1)
-            prefix=(prefix*base)%mod
-            prefix=(prefix+char)%mod
+        mod=10**9+7
 
-            suffix=(suffix+char*power)%mod
+        for i, c in enumerate(s):
+            char=(ord(c)-ord("a")+1) #offset by 1 so S starts with 1
+
+            prefix=(prefix * base) %mod
+            prefix=(prefix + char) %mod
+
+            suffix=(suffix+char*power)% mod
             power=(power*base) %mod
+
             if prefix==suffix:
                 last_index=i
+
+
         suffix=s[last_index+1:]
         return suffix[::-1]+s
-
-
-
-
-
-
-
