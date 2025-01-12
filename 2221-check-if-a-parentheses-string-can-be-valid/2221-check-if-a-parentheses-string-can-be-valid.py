43bc043dad737e212ec0fa811b17738485b2442e
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-01-12 08:33:22 +0530

    Time: 67 ms (86.74%), Space: 22.3 MB (6.05%) - LeetHub

---
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canBeValid(self, s: str, locked: str) -> bool:
+        unlocked=[]
+        stack_locked=[]
+        for i in range(len(s)):
+            if locked[i]=="0":
+                unlocked.append(i)
+            elif s[i]=="(":
+                stack_locked.append(i)
+            else:
+                if stack_locked:
+                    stack_locked.pop()
+                elif unlocked:
+                    unlocked.pop()
+                else:
+                    return False
+        while stack_locked and unlocked and stack_locked[-1]<unlocked[-1]:
+            stack_locked.pop()
+            unlocked.pop()
+        
+        if stack_locked:
+            return False
+        return True if len(unlocked)%2==0 else False
