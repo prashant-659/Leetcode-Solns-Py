bc05c8e983f132767b21730c4cbbaf8915d70171
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-01-13 08:47:32 +0530

    Time: 147 ms (41.27%), Space: 19 MB (14.92%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.py
@@ -3,11 +3,10 @@ class Solution:
         if len(s)<3:
             return len(s)
         c=Counter(s)
-        res=0
         dup=0
         for x in c:
             if c[x]>2:
-                dup+=c[x]-(1 if c[x]%2 else 2)
+                dup+=c[x]-(1 if c[x]&1 else 2)
         return len(s)-dup
         
 
