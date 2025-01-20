9b7d642f661913c57c47e574f484bc82c5d6abaa
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-01-20 12:35:41 +0530

    Time: 114 ms (73.5%), Space: 51.9 MB (53.31%) - LeetHub

---
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.py
@@ -0,0 +1,22 @@
+class Solution:
+    def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
+        R=len(mat)
+        C=len(mat[0])
+
+        mat_to_pos={}
+        for r in range(R):
+            for c in range(C):
+                mat_to_pos[mat[r][c]]=(r,c)
+
+        row_cnt=[0]*R
+        col_cnt=[0]*C
+        ans=0
+        for i in range(len(arr)):
+            r,c =mat_to_pos[arr[i]]
+            row_cnt[r]+=1
+            col_cnt[c]+=1
+             
+            if col_cnt[c]==R or row_cnt[r]==C:
+                ans=i
+                break
+        return ans
