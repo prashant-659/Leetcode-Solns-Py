761a97e28411a935114b596509bedf87686f5f78
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-01-22 13:02:39 +0530

    Time: 708 ms (79.76%), Space: 85 MB (48.23%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.py
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.py
@@ -3,22 +3,31 @@ class Solution:
         R=len(isWater)
         C=len(isWater[0])
         #multisourced bfs
-        visit=set()
         q=deque()
+        res=[[-1] *C for _ in range(R)]
+        #enqueue all water cells
         for r in range(R):
             for c in range(C):
                 if isWater[r][c]:
-                    q.append((r,c, 0))
+                    q.append((r,c))
                     isWater[r][c]=0
-                    visit.add((r,c))
+                    res[r][c]=0
         while q:
-            r,c,h=q.popleft()
+            r,c=q.popleft()
+            h=res[r][c]
 
             nei=[(r+1,c),(r-1,c),(r,c+1),(r,c-1)]
             for nr, nc in nei:
-                if nr<0 or nr==R or nc<0 or nc==C or (nr,nc) in visit:
+                if nr<0 or nr==R or nc<0 or nc==C or  res[nr][nc]!=-1:
                     continue
-                q.append((nr, nc, h+1))
-                isWater[nr][nc]=h+1
-                visit.add((nr,nc))
-        return isWater
+                q.append((nr, nc))
+                res[nr][nc]=h+1
+                
+        return res
+
+
+
+
+
+
+
