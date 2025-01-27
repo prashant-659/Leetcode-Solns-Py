69355fc4a6f6b6315580ce49a98279fc9ad428cd
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-01-28 00:13:45 +0530

    Time: 3 ms (93.46%), Space: 19.3 MB (40.19%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
@@ -1,14 +1,29 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        edges={i:graph[i] for i in range(len(graph))}
+        # edges={i:graph[i] for i in range(len(graph))}
 
-        res=[] 
-        stack=[(0,[0])]
-        while stack:
-            node, path=stack.pop()
-            if node==len(graph)-1:
-                res.append(path)
+        # res=[] 
+        # stack=[(0,[0])]
+        # while stack:
+        #     node, path=stack.pop()
+        #     if node==len(graph)-1:
+        #         res.append(path)
             
-            for nei in edges[node]:
-                stack.append((nei, path+[nei]))
-        return res
+        #     for nei in edges[node]:
+        #         stack.append((nei, path+[nei]))
+        # return res
+        result=[]
+        def dfs(node, path):
+
+        # If the current node is the target, add the path to the result
+            if node == len(graph)-1:
+                result.append(path.copy())
+                return
+            
+            # Explore all neighbors
+            for neighbor in graph[node]:
+                path.append(neighbor)  # Add the neighbor to the current path
+                dfs(neighbor, path)    # Recursively explore
+                path.pop() 
+        dfs(0, [0])     # Start DFS from node 0 with the initial path [0]
+        return result
