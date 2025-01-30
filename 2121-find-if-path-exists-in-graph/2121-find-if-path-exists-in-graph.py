b4cd0408dbb69ca3ba70fc2e912a2300053c2c9b
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-01-30 23:35:58 +0530

    Time: 303 ms (90.12%), Space: 96.5 MB (71.22%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
@@ -1,20 +1,45 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        graph = collections.defaultdict(list)
+        # graph = collections.defaultdict(list)
+        # for u, v in edges:
+        #     graph[u].append(v)
+        #     graph[v].append(u)
+        
+        # queue = deque([source])
+        # visited = set([source])
+        
+        # while queue:
+        #     node = queue.popleft()
+        #     if node == destination:
+        #         return True
+        #     for neighbor in graph[node]:
+        #         if neighbor not in visited:
+        #             visited.add(neighbor)
+        #             queue.append(neighbor)
+        
+        # return False
+
+
+
+
+
+
+
+        adjList=defaultdict(list)
         for u, v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
-        
-        queue = deque([source])
-        visited = set([source])
-        
-        while queue:
-            node = queue.popleft()
-            if node == destination:
+            adjList[u].append(v)
+            adjList[v].append(u)
+        q=deque([source])
+        visit=set()
+        visit.add(source)
+
+        while q:
+            node=q.popleft()
+            if node==destination:
                 return True
-            for neighbor in graph[node]:
-                if neighbor not in visited:
-                    visited.add(neighbor)
-                    queue.append(neighbor)
-        
+            for nei in adjList[node]:
+                if nei in visit:
+                    continue
+                q.append(nei)
+                visit.add(nei)
         return False
