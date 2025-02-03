06a238660f22a216f65e322a5d84e6c7d131fb1f
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-02-03 23:55:51 +0530

    Time: 20 ms (95.71%), Space: 23.9 MB (35.46%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
@@ -1,22 +1,47 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        safe={}
+        # safe={}
         
-        def dfs(i):
-            if i in safe:
-                return safe[i]
-            safe[i]=False
-            for nei in graph[i]:
-                if not dfs(nei):
-                    return safe[i]
-            safe[i]=True
-            return safe[i]
+        # def dfs(i):
+        #     if i in safe:
+        #         return safe[i]
+        #     safe[i]=False
+        #     for nei in graph[i]:
+        #         if not dfs(nei):
+        #             return safe[i]
+        #     safe[i]=True
+        #     return safe[i]
 
-        res=[]
+        # res=[]
+        # for i in range(len(graph)):
+        #     if dfs(i):
+        #         res.append(i)
+        # return res
+        visited=[0]*len(graph)
+        currPath=[0]*len(graph)
+
+        def dfs(node):
+            visited[node]=1
+            currPath[node]=1
+            for nei in graph[node]:
+                if not visited[nei]:
+                    ans=dfs(nei)
+                    if ans==True:
+                        return True
+                else:
+                    if currPath[nei]==1:
+                        return True
+            currPath[node]=0
+            return False
+        ans=[]
         for i in range(len(graph)):
-            if dfs(i):
-                res.append(i)
-        return res
-        
+            if not visited[i]:
+                dfs(i)
+        for i in range(len(currPath)):
+            if currPath[i]==0:
+                ans.append(i)
+        return ans
+            
+
 
         
