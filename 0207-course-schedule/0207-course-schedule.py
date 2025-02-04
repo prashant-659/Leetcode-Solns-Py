5e9ee09269ea339afd11f23f179f6ac9396fe3dc
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-02-04 23:03:54 +0530

    Time: 0 ms (100%), Space: 18.8 MB (85.4%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -22,27 +22,47 @@ class Solution:
         for u, v in prereq:
             graph[u].append(v)
 
-        visit,cycle=set(),set()
-        res=[]
-        def dfs(crs):
-            if crs in cycle:
-                return False
-            if crs in visit:
-                return True
-            cycle.add(crs)
-            for child in graph[crs]:
-                if not dfs(child):
-                    return False
-            cycle.remove(crs)
-            visit.add(crs)
-            return True
+        # visit,cycle=set(),set()
+        # res=[]
+        # def dfs(crs):
+        #     if crs in cycle:
+        #         return False
+        #     if crs in visit:
+        #         return True
+        #     cycle.add(crs)
+        #     for child in graph[crs]:
+        #         if not dfs(child):
+        #             return False
+        #     cycle.remove(crs)
+        #     visit.add(crs)
+        #     return True
         
 
 
 
             
 
-        for crs in range(numCourses):
-            if not dfs(crs):
-                return False
+        # for crs in range(numCourses):
+        #     if not dfs(crs):
+        #         return False
+        # return True
+
+        indegree=[0]*numCourses
+        for i in range(len(graph)):
+            for x in graph[i]:
+                indegree[x]+=1
+        q=deque()
+        for i in range(numCourses):
+            if indegree[i]==0:
+                q.append(i)
+        
+        while q:
+            course=q.popleft()
+            for nei in graph[course]:
+                indegree[nei]-=1
+                if indegree[nei]==0:
+                    q.append(nei)
+        if max(indegree)!=0:
+            return False
         return True
+        
