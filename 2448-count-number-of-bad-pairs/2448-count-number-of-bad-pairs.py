83444e8a8d8eb8a3218f340e65272841d55b756a
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-02-09 08:52:25 +0530

    Time: 98 ms (29.28%), Space: 38.9 MB (28.81%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.py
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.py
@@ -1,15 +1,27 @@
 class Solution:
     def countBadPairs(self, nums: List[int]) -> int:
 
-        n = len(nums)
-        res = []
-        for i in range(n):
-            res.append(nums[i] - i)
+        # n = len(nums)
+        # res = []
+        # for i in range(n):
+        #     res.append(nums[i] - i) #finding the sslope
 
-        a = Counter(res)
-        ans = n * (n - 1) // 2
-        for x in a:
-            if a[x] > 1:
-                ans -= a[x] * (a[x] - 1) // 2
+        # a = Counter(res) #good pairs hashmap
+        # ans = n * (n - 1) // 2
+        # for x in a:
+        #     if a[x] > 1:
+        #         ans -= a[x] * (a[x] - 1) // 2
         
-        return ans
+        # return ans
+
+        good_pairs=0
+        total_pairs=0
+        ct=defaultdict(int)
+        for i in range(len(nums)):
+            total_pairs+=i
+            good_pairs+=ct[nums[i]-i]
+            ct[nums[i]-i]+=1
+
+
+
+        return total_pairs-good_pairs
