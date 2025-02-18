6dcc2da7cf1ea4b3bcd818aeb5afe734990b830e
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-02-19 00:06:49 +0530

    Time: 711 ms (33.91%), Space: 19.7 MB (53.83%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,15 +1,16 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
+        n=len(s)
         s2=s[::-1]
-        dp=[[0 for j in range((len(s2)+1))] for i in range(len(s)+1)]
-        for i in range(1, len(s)+1):
-            for j in range(1, len(s2)+1):
+        dp=[[0 for j in range((n+1))] for i in range(n+1)]
+        for i in range(1, n+1):
+            for j in range(1, n+1):
                 if s[i-1]==s2[j-1]:
                     dp[i][j]=1+dp[i-1][j-1]
                 else:
                     dp[i][j]=max(dp[i-1][j], dp[i][j-1])
-        x=dp[len(s)][len(s)]
-        return len(s)-x
+        x=dp[n][n]
+        return n-x
         # if max(dp)==len(s)+1:
         #     return 0
         # max_=max(dp)
