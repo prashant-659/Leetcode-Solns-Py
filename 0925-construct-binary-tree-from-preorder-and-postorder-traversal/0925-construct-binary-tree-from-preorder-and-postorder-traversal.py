e3aac12fd1e90096e9231fdb6371588075e5ce5b
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-02-23 08:57:05 +0530

    Time: 3 ms (50%), Space: 18 MB (10.17%) - LeetHub

--- 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.py
+++ 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -6,22 +6,22 @@
 #         self.right = right
 class Solution:
     def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
-        dic={n:i for i, n in enumerate(postorder)}
-        def dfs(pre, preStart, preEnd, post, postStart, posEnd):
-            if preStart>preEnd: return None
+        n=len(postorder)
+        post_val_to_idx={n:i for i, n in enumerate(postorder)}
 
-            root=TreeNode(pre[preStart])
-            if preStart==preEnd:
-                return root
+        def build(i1,i2,j1,j2):
+            if i1>i2 or j1>j2:
+                return None
+            root=TreeNode(preorder[i1])
+            if i1!=i2: #root or node found
+                left_val=preorder[i1+1]
+                mid=post_val_to_idx[left_val]
+                left_size=mid-j1+1 
 
-            postIndex= postStart
-            # while post[postIndex]!=pre[preStart+1]: 
-            #     postIndex+=1
-            postIndex=dic[pre[preStart+1]]
+                root.left=build(i1+1, i1+left_size, j1, mid)
+                root.right=build(i1+left_size+1, i2, mid+1, j2-1)
+                
 
-            length=postIndex-postStart+1
-            root.left=dfs(pre, preStart+1, preStart+length, post, postStart, postIndex)
-            root.right=dfs(pre, preStart+length+1, preEnd, post, postIndex+1, posEnd-1)
             return root
-        return dfs(preorder, 0, len(preorder)-1, postorder, 0, len(preorder)-1)
 
+        return build(0,n-1,0,n-1)
