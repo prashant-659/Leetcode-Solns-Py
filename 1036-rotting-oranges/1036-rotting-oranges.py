85006de56af94aca5309e9be22e810786b94a524
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-02-25 20:20:59 +0530

    Time: 3 ms (82.73%), Space: 17.7 MB (72.2%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,32 +1,29 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        
-        time, fresh=0,0
-        R,C=len(grid), len(grid[0])
-        q=collections.deque()
-        
-
-        for r in range(R):
-            for c in range(C):
+        m, n =len(grid), len(grid[0])
+        q=deque()
+        fresh=0
+        for r in range(m):
+            for c in range(n):
                 if grid[r][c]==1:
                     fresh+=1
                 if grid[r][c]==2:
-                    q.append([r,c])
-
-        directions=[[0,1],[0,-1],[1,0],[-1,0]]
+                    q.append((r,c))
+        dir=[(1,0),(-1,0),(0,1),(0,-1)]
+        time=0
         while q and fresh>0:
             for i in range(len(q)):
-                r,c=q.popleft()
-                for dr, dc in directions:
-                    row, col=r+dr, dc+c
+                r, c=q.popleft()
+            
 
-                    if (row<0 or row==len(grid) or
-                       (col<0 or col==len(grid[0])) or
-                       grid[row][col]!=1):
-                       continue
-
-                    grid[row][col]=2
-                    q.append([row,col])
-                    fresh-=1
+                for dr, dc in dir:
+                    nr, nc=r+dr, c+dc
+                    if nr==m or nc==n or nr<0 or nc<0 or grid[nr][nc]!=1:
+                        continue
+                    grid[nr][nc]=2
+                    q.append((nr, nc))
+                    fresh-=1    
+                
+                
             time+=1
         return time if fresh==0 else -1
