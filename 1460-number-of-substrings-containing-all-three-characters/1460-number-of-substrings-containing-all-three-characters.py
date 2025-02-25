a4c5afd2b787e2fc8c22d28752fbd19feb016e56
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-02-26 00:36:09 +0530

    Time: 72 ms (87.69%), Space: 17.9 MB (66.6%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
@@ -1,16 +1,29 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        mp=defaultdict(int)
-        l=0
-        count=0
-        for r in range(len(s)):
-            mp[s[r]]+=1
+        # mp=defaultdict(int)
+        # l=0
+        # count=0
+        # for r in range(len(s)):
+        #     mp[s[r]]+=1
 
-            while len(mp)==3:
-                count+=(len(s)-r)
-                mp[s[l]]-=1
-                if mp[s[l]]==0:
-                    del mp[s[l]]
-                l+=1
+        #     while len(mp)==3:
+        #         count+=(len(s)-r) # number of valid substrings ending at index r
+        #         mp[s[l]]-=1
+        #         if mp[s[l]]==0:
+        #             del mp[s[l]]
+        #         l+=1
                       
+        # return count
+        last = {'a': -1, 'b': -1, 'c': -1} 
+        count = 0
+        
+        for j in range(len(s)):
+            last[s[j]] = j  
+            
+            min_last = min(last['a'], last['b'], last['c'])
+            
+            # If all three characters are , count valid substrings
+            if min_last != -1:
+                count += (min_last + 1)
+        
         return count
