c403df9d587390f55bb4a7af46dff68555b63b6a
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-02-26 09:13:05 +0530

    Time: 104 ms (17.81%), Space: 28.5 MB (34.63%) - LeetHub

--- 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.py
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.py
@@ -2,17 +2,19 @@ class Solution:
     def maxAbsoluteSum(self, nums: List[int]) -> int:
         cur=0
         maxi=-math.inf
+        cur2=0 
+        max2=math.inf
         for n in nums:
             cur=max(cur,0)
             cur+=n
             maxi=max(cur, maxi)
-        cur=0 
-        max2=math.inf
-        for n in nums:
-            cur=min(cur, 0)
 
-            cur+=n
-            max2=min(cur, max2)
+            cur2=min(cur2, 0)
+
+            cur2+=n
+            max2=min(cur2, max2)
+        
+            
         return max(maxi, abs(max2))
         
             
