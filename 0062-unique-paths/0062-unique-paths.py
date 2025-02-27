a227bab198fa65947d35a42fe828cbe2e8d66924
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-02-27 12:58:17 +0530

    Time: 37 ms (2.01%), Space: 18 MB (10.07%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,17 +1,19 @@
+from math import comb
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        ans=0
-        dp={(0,0):1}
-        def move(d,r):
-            if d==0 and r==0:
-                return 1
-            if d<0 or r<0:
-                return 0
-            if (d,r) in dp:
-                return dp[(d,r)]
-            up=move(d-1,r)
-            left=move(d, r-1)
-            dp[(d,r)]=up+left
-            return dp[(d,r)]
-        move(m-1,n-1)
-        return dp[(m-1,n-1)]
+        # ans=0
+        # dp={(0,0):1}
+        # def move(d,r):
+        #     if d==0 and r==0:
+        #         return 1
+        #     if d<0 or r<0:
+        #         return 0
+        #     if (d,r) in dp:
+        #         return dp[(d,r)]
+        #     up=move(d-1,r)
+        #     left=move(d, r-1)
+        #     dp[(d,r)]=up+left
+        #     return dp[(d,r)]
+        # move(m-1,n-1)
+        # return dp[(m-1,n-1)]
+        return comb(m+n-2,m-1)
