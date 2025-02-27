c3c07fa73f28da3ea9d06f4c94ec6583965a1ade
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-02-27 13:38:22 +0530

    Time: 0 ms (100%), Space: 17.7 MB (88.45%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -16,4 +16,12 @@ class Solution:
         #     return dp[(d,r)]
         # move(m-1,n-1)
         # return dp[(m-1,n-1)]
-        return comb(m+n-2,m-1)
+        dp=[[0] *(n+1) for _ in range(m+1)]
+        for i in range(m+1):
+            dp[i][0]=1
+        for i in range(n+1):
+            dp[0][i]=1
+        for i in range(1,m+1):
+            for j in range(1, n+1):
+                dp[i][j]=dp[i-1][j]+dp[i][j-1]
+        return dp[m-1][n-1]
