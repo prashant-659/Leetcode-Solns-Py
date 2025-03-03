783c8c38118b6cec1087a7fcaa4452cad74391c0
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-03-03 15:40:48 +0530

    Time: 1283 ms (19.45%), Space: 0B (100%) - LeetHub

--- 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -1,27 +1,15 @@
-SELECT
-  product_id,
-  10 AS price
-FROM
-  Products
-GROUP BY
-  product_id
-HAVING
-  MIN(change_date) > '2019-08-16'
-UNION ALL
-SELECT
-  product_id,
-  new_price AS price
-FROM
-  Products
-WHERE
-  (product_id, change_date) IN (
-    SELECT
-      product_id,
-      MAX(change_date)
-    FROM
-      Products
-    WHERE
-      change_date <= '2019-08-16'
-    GROUP BY
-      product_id
-  )
+# Write your MySQL query statement below
+with cte as(
+    select distinct product_id, max(change_Date) over(partition by product_id) as last_day
+    from products where change_Date<="2019-08-16"
+
+)
+select c.product_id, p.new_price as price
+from cte c
+join products p
+on c.product_id=p.product_id and c.last_day=change_date
+
+union
+select product_id, 10 as price
+from products
+where product_id not in (select product_id from cte)
