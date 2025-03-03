e1b002dc49e3d7b4a2efa6fc050a641cfe7f4101
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-03-03 12:13:57 +0530

    Time: 524 ms (42.63%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,8 +1,9 @@
 # Write your MySQL query statement below
-with cte as(
-    select salary,DENSE_RANK() OVER (
-    ORDER BY salary DESC
-  ) salary_rank
-    from employee
-)
-select max(salary) as SecondHighestSalary  from cte where salary_rank=2;
+-- with cte as(
+--     select salary,DENSE_RANK() OVER (
+--     ORDER BY salary DESC
+--   ) salary_rank
+--     from employee
+-- )
+-- select max(salary) as SecondHighestSalary  from cte where salary_rank=2;
+select max(salary) as SecondHighestSalary from employee where salary <(select max(salary) from employee )
