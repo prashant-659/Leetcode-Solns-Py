3a3cddf7d594d1dddf293fe85c81b0684b6f4b1d
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-03-04 13:25:32 +0530

    Time: 1732 ms (28.76%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -1,7 +1,24 @@
 # Write your MySQL query statement below
-select p.product_id,  IFNULL(ROUND(SUM(units*price)/SUM(units),2),0) AS average_price
-from prices p 
-left join unitsSold u 
+with unitscost as ( 
+    select p.product_id, sum(u.units) as total_units,sum(p.price*u.units) as total_price
+from prices p
+left join unitssold u
 on p.product_id=u.product_id
-and u.purchase_date BETWEEN start_date AND end_date
-group by p.product_id;
+and u.purchase_date between p.start_date and p.end_Date
+group by p.product_id)
+select product_id, ifnull(round(total_price/total_units,2),0) as average_price
+from unitscost
+
+-- WITH unitscost AS ( 
+--     SELECT p.product_id, 
+--            SUM(u.units) AS total_units, 
+--            SUM(p.price * u.units) AS total_price
+--     FROM prices p
+--     LEFT JOIN unitssold u ON p.product_id = u.product_id
+--     AND u.purchase_date BETWEEN p.start_date AND p.end_date
+--     GROUP BY p.product_id
+-- )
+-- SELECT product_id, 
+--        ROUND(total_price / total_units, 2) AS average_price
+-- FROM unitscost;
+
