6807bd96f00d91c083fb9c148843ec066504488b
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-03-05 23:01:45 +0530

    Time: 30 ms (30.06%), Space: 17.9 MB (56.3%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -15,7 +15,7 @@ class Solution:
             cur.pop()
 
             #skip
-            while i<len(candidates)-1 and candidates[i]==candidates[i+1]:
+            while i+1<len(candidates) and candidates[i]==candidates[i+1]:
                 i+=1
             dfs(i+1, cur, total)
 
