3e5c5e753cce93df0664d31afee0a30fefca9807
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-03-05 20:16:01 +0530

    Time: 1527 ms (23.42%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,11 +1,32 @@
-# Write your MySQL query statement below
-with cte as
-(
-    select customer_id,delivery_id,order_date,customer_pref_delivery_date,
-    row_number() over(partition by customer_id order by order_date) as firstorder
-    from delivery
+-- # Write your MySQL query statement below
+-- with first_order as(
+--     select customer_id, min(order_date) as firstorder
+--     from delivery
+--     group by customer_id
+-- ),
+-- immediate as(
+--     select d.customer_id, f.firstorder
+--     from first_order f
+--     join delivery d
+--     on f.customer_id=d.customer_id
+--     where f.firstorder=d.customer_pref_delivery_date 
+-- ),
+-- sele
+
+WITH first_order AS (
+    SELECT customer_id, MIN(order_date) AS firstorder
+    FROM delivery
+    GROUP BY customer_id
+),
+immediate AS (
+    SELECT d.customer_id, f.firstorder
+    FROM first_order f
+    JOIN delivery d
+    ON f.customer_id = d.customer_id
+    WHERE f.firstorder = d.customer_pref_delivery_date
 )
-select round(
-    100*sum(case when order_date=customer_pref_delivery_date then 1 else 0 end)/count(*),2)  as immediate_percentage
-from cte 
-where firstorder=1;
+
+SELECT round(COUNT(*) *100/ NULLIF((SELECT COUNT(*) FROM first_order), 0),2) AS immediate_percentage 
+FROM immediate;
+
+
