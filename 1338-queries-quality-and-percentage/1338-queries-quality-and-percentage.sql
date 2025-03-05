bfee22beefcffc08228aac028bd0b4a90e56f374
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-03-05 12:40:47 +0530

    Time: 761 ms (32.88%), Space: 0B (100%) - LeetHub

--- 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
@@ -1,12 +1,4 @@
 # Write your MySQL query statement below
--- SELECT distinct query_name,
---                 ROUND(
---                     (sum(rating/position)/count(query_name))
---                     ,2) as quality,
---                 Round(sum(rating from (select rating from queries where rating<3))/count(query_name),2) as poor_query_percentage 
--- From queries 
--- group by query_name
-
 SELECT DISTINCT 
     query_name,
     ROUND(SUM(rating/position) / COUNT(query_name), 2) AS quality,
@@ -16,4 +8,3 @@ FROM
 Where query_name is not null
 GROUP BY 
     query_name ;
-
