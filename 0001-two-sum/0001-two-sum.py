c7d7a0eeb9fed4e50817619250280bd248373d55
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-03-05 23:13:00 +0530

    Time: 4 ms (42.2%), Space: 18.9 MB (50.94%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mp={}
+        for i in range(len(nums)):
+            if target-nums[i] in mp:
+                return [i, mp[target-nums[i]]]
+            mp[nums[i]]=i
+        
+        
