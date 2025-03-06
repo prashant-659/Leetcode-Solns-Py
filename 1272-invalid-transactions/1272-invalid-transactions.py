ae4d76c6ade99d1706d56b4e06382e49d840af8f
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-03-06 20:20:03 +0530

    Time: 258 ms (7.52%), Space: 18.2 MB (89.95%) - LeetHub

---
+++ 1272-invalid-transactions/1272-invalid-transactions.py
@@ -0,0 +1,28 @@
+class Solution:
+    def invalidTransactions(self, transactions: List[str]) -> List[str]:
+        n=len(transactions)
+        name=[""]*n
+        amount=[0]*(n)
+        city=[""]*n
+        time=[0]*n
+        for i,t in enumerate(transactions):
+            cur=t.split(",")
+            na,t,a,ci=cur[0],cur[1],cur[2],cur[3]
+            name[i]=na
+            time[i]=int(t)
+            amount[i]=int(a)
+            city[i]=ci
+        index=set()
+        for i in range(n):
+            if amount[i]>1000:
+                index.add(i)
+            for j in range(n):
+                if i==j:
+                    continue
+                if  name[i]==name[j] and city[i]!=city[j] and abs(time[i]-time[j])<=60:
+                    index.add(j)
+                    index.add(i)
+        res=[transactions[i] for i in index]
+        
+        return res
+                
