89083f030449f257d065b4b2ae06e47d3569616b
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-03-07 23:14:47 +0530

    Time: 2336 ms (9.57%), Space: 25.7 MB (18.28%) - LeetHub

--- 0218-the-skyline-problem/0218-the-skyline-problem.py
+++ 0218-the-skyline-problem/0218-the-skyline-problem.py
@@ -1,30 +1,22 @@
-from sortedcontainers import SortedList
 class Solution:
     def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
-        sh=[]
-        for x, y, h in buildings:
-            sh.append((x,h, 0))
-            sh.append((y,h,1))
-        sh.sort()
+        heap=[0]
 
+        sweep=[]
+        for l, r, h in buildings:
+            sweep.append([l, -h])
+            sweep.append([r, h])
+        sweep.sort()
 
-        active_heights=SortedList([0])
         ans=[]
-        n=len(sh)
-        i=0
-        while i<n:
-            cur_x=sh[i][0] 
-
-            #process events with same x together
-            while i<n and sh[i][0]==cur_x:
-                x,h,t=sh[i]
-                if t==0:
-                    active_heights.add(h)
-                else:
-                    active_heights.remove(h)
-                i+=1
-        # check if heights has changed
-
-            if not ans or ans[-1][1]!=active_heights[-1]:
-                ans.append([cur_x, active_heights[-1]])
+        for e, h in sweep:
+            if  h<0:
+                if h<heap[0]:
+                    ans.append([e,-h])
+                heapq.heappush(heap, h)
+            else:
+                heap.remove(-h)
+                heapify(heap)
+                if -h<heap[0]:
+                    ans.append([e, -heap[0]])
         return ans
