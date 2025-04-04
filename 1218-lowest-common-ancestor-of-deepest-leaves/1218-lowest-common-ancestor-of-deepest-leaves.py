a2536628481dbdec5376edb3657e9be1298dabdb
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-04-04 08:32:50 +0530

    Time: 0 ms (100%), Space: 18.1 MB (92.69%) - LeetHub

--- 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.py
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.py
@@ -4,33 +4,20 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# class Solution:
-#     def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-#         def dfs(root):
-#             if not root:
-#                 return 0, None
-#             d1, n1=dfs(root.left)
-#             d2, n2 = dfs(root.right) 
-#             if d1==d2:
-#                 return d1+1, root
-#             elif d1>d2:
-#                 return d1+1, n1
-#             else:
-#                 return d2+1, n2
-#         d, n=dfs(root)
-#         return n
-
 class Solution:
-    def lcaDeepestLeaves(self, root: TreeNode) -> TreeNode:
+    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         def dfs(node):
             if not node:
-                return (0, None)
-            left_depth, left_node = dfs(node.left)
-            right_depth, right_node = dfs(node.right)
-            if left_depth == right_depth:
-                return (left_depth + 1, node)
-            elif left_depth > right_depth:
-                return (left_depth + 1, left_node)
+                return 0, None
+            d1, n1 = dfs(node.left)
+            d2, n2  = dfs(node.right)
+
+            if d1==d2:
+                return d1+1, node
+            elif d1>d2:
+                return d1+1,  n1
             else:
-                return (right_depth + 1, right_node)
-        return dfs(root)[1]
+                return d2+1, n2
+        d, n=dfs(root)
+        return n
+            
