bdaa01a77cc949d41da73437953aefdaea61e314
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-05-13 07:52:04 +0530

    Time: 1965 ms (69.93%), Space: 93.8 MB (11.11%) - LeetHub

--- 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.py
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.py
@@ -1,16 +1,14 @@
 class Solution:
     def lengthAfterTransformations(self, s: str, t: int) -> int:
-        mod=10e9+7
         @cache
-        def helper(s,t):
-            ans=0
+        def helper(s, t):
+            ans = 0
             for c in s:
-                if ord(c)+t<=ord("z"):
-                    ans+=1
+                if ord(c) + t <= ord("z"):
+                    ans += 1
                 else:
-                    new_t=t
-                    new_t-=ord("z")+1-ord(c)
-                    ans+=helper("ab",new_t)
+                    new_t = t
+                    new_t -= ord("z") + 1 - ord(c)
+                    ans += helper("ab", new_t)
             return ans
-        return helper(s,t)%((10**9)+7)
-
+        return helper(s, t) % ((10 ** 9) + 7)
