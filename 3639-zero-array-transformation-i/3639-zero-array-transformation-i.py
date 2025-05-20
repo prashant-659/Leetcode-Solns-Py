47b2dadbdec1dfef4815d10b637b1287f8ec1d28
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-05-20 07:57:02 +0530

    Time: 70 ms (84.84%), Space: 54.9 MB (90.08%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.py
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.py
@@ -8,30 +8,4 @@ class Solution:
             arr[rght+1]-= 1
 
         return all(num <= acc for num, acc in zip(nums, accumulate(arr)))         # if max(nums)>len(queries):
-        #     return False
-        # if queries==[[1,1],[1,1],[0,1],[1,1],[0,0],[0,0],[0,1],[1,1],[1,1]] and nums==[5,4]:
-        #     return False
-        # return True
-        # queries.sort()
-        # prefixSum=[]
-        # prefixDiff=[]
-        # for i in range(len(nums)):
-        #     # l=queries[0]
-        #     # r=queries[1]
-        #     # while l<=r:
-        #     #     if nums[l]!=
-
-        # prefix=[0]*(len(nums)+1)
-        # for l, r in enumerate(queries):
-        #     prefix[l]-=1
-        #     prefix[r+1]+=1
-        
-        # for i in range(1, nums):
-        #     prefix[i]+=prefix[i-1]
-        
-        # for i in range(len(nums)):
-        #     if (prefix[i]+nums[i])>0:
-        #         return False
-
-        # return True
-
+       
