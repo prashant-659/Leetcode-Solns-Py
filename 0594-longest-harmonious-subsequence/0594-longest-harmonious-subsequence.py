047bc8540c8d85e4d9494c634aaee4d48979dd12
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-06-30 07:44:29 +0530

    Time: 58 ms (17.99%), Space: 18.9 MB (93.48%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -1,19 +1,5 @@
 # class Solution:
 #     def findLHS(self, nums: List[int]) -> int:
-#         # mapped=defaultdict(int)
-#         # 
-#         # for i in nums:
-
-#         nums.sort()
-#         l,r=0,0
-#         res=0
-#         while r<len(nums):
-#             while nums[r]-nums[l]>1:
-#                 l+=1
-#             if nums[r]-nums[l]==1:
-#                 res=max(res,l-r+1)
-#             r+=1
-#         return res
 
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
