61c1aca108fc3db15a4fbcb80eebd354b2f82e12
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-07-19 08:12:06 +0530

    Time: 39 ms (69.82%), Space: 30.7 MB (53.03%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.py
@@ -1,69 +1,29 @@
-class Trie:
-    def __init__(self):
-        self.children={} #string -> Trie
-        self.end_folder=False
-    def add(self,path):
-        cur=self
-        for f in path.split("/"):
-            if f not in cur.children:
-                cur.children[f]=Trie()
-            cur=cur.children[f]
-        cur.end_folder=True
-
-    def prefix_search(self,path):
-        cur=self
-        folders=path.split("/")
-
-        for i in range(len(folders)-1):
-            cur=cur.children[folders[i]]
-            if cur.end_folder:
-                return True
-        return False
-        
-
-
 class Solution:
-    def removeSubfolders(self, folder: List[str]) -> List[str]:
-        trie=Trie()
+    def removeSubfolders(self, folder) -> list[str]:
+        # Create a set to store all folder paths for fast lookup
+        folder_set = set(folder)
+        result = []
 
-
+        # Iterate through each folder to check if it's a sub-folder
         for f in folder:
-            trie.add(f)
-        
-        res=[]
-        for f in folder:
-            if not trie.prefix_search(f):
-                res.append(f)
-        return res
+            is_sub_folder = False
+            prefix = f
 
+            # Check all prefixes of the current folder path
+            while not prefix == "":
+                pos = prefix.rfind("/")
+                if pos == -1:
+                    break
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # folder_set=set(folder)
-        # res=[]
+                # Reduce the prefix to its parent folder
+                prefix = prefix[0:pos]
 
-        # # O(n*L2)
-        # for f in folder:
-        #     res.append(f)
-        #     for i in range(len(f)):
-        #         if f[i]=="/" and f[:i] in folder_set:
-        #             res.pop()
-        #             break
-        # return res
+                # If the parent folder exists in the set, mark as sub-folder
+                if prefix in folder_set:
+                    is_sub_folder = True
+                    break
 
+            # If not a sub-folder, add it to the result
+            if not is_sub_folder:
+                result.append(f)
+        return result
