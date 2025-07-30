32d097c5c0d8991cf5fb69db09542358e1479260
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-07-30 09:20:56 +0530

    Time: 66 ms (49.11%), Space: 31.4 MB (8.28%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.py
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.py
@@ -1,43 +1,9 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        # max_val=res=curr=0
-        # for i in range(len(nums)):
-        #     if max_val<nums[i]:
-        #         max_val=nums[i]
-        #         res=curr=0
-        #     if max_val==nums[i]:
-        #         curr+=1
-        #         res=max(curr,res)
-        #     else:
-        #         curr=0
-        # return res
-
-        #find max amd increse size if they are consecutive
-        # target=max(nums)
-        # size=0
-        # res=0
-        # for n in nums:
-        #     if n==target:
-        #         size+=1
-        #     else:
-        #         size=0
-        #     res=max(res,size)
-        # return res
-
-        #NeetCode IO
-        #1 If n< curr_max, n & curr_max < curr_max
-        #1 If n== curr_max, n & curr_max = curr_max
-        #1 If n> curr_max, n & curr_max < curr_max
-        size, res=0,0
-        cur_max=0
-        for n in nums:
-            if n>cur_max:
-                cur_max=n
-                size=1
-                res=0
-            elif n==cur_max:
-                size+=1
-            else:
-                size=0
-            res=max(res,size)
-        return res
+        max_val = max(nums)
+        max_streak = max(
+            len(list(values))
+            for key, values in groupby(nums)
+            if key == max_val
+        )
+        return max_streak
