b7284d5c00176c39edc8245691375043e6ca8a1c
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-08-20 11:47:41 +0530

    Time: 122 ms (21.69%), Space: 20.2 MB (90.78%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
@@ -1,15 +1,11 @@
 class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
-        
-        count=matrix.count(1)
-        count=0
-        for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
-                if matrix[r][c]==1:
-                    count+=1
-                if r==0 or c==0: continue
-                
-                old_val=matrix[r][c]
-                matrix[r][c]=min(matrix[r-1][c-1], matrix[r][c-1],matrix[r-1][c]) + 1 if matrix[r][c]==1 else 0
-                count= count+ matrix[r][c]- old_val  
-        return count
+        n, m = len(matrix), len(matrix[0])
+        ans = sum(matrix[0])
+        for i in range(1, n):
+            ans += matrix[i][0]
+            for j in range(1, m):
+                if matrix[i][j]:
+                    matrix[i][j] += min(matrix[i-1][j-1], matrix[i][j-1], matrix[i-1][j])
+                    ans += matrix[i][j]
+        return ans     
