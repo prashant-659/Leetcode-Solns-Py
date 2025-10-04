269adaec08943f43a8806502750a9e1f15d27239
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-10-04 08:42:10 +0530

    Time: 113 ms (13.71%), Space: 28.5 MB (68.11%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,23 +1,22 @@
 class Solution:
-    def maxArea(self, h: List[int]) -> int:
-        # l,r=0,len(height)-1
-        # area=0
-        # while l<r:
-        #     area=max(area,min(height[l],height[r])*(r-l))
-        #     if height[l]<height[r]:
-        #         l+=1
-        #     else:
-        #         r-=1
-            
-        # return area
-        l=0
-        r=len(h)-1
-        #max_area=0
-        curr_area=0
+    def maxArea(self, height: List[int]) -> int:
+#         left=[0]*len(height)
+#         left[0]=height[0]
+#         right=[0]*len(height)
+#         right[-1]=height[-1]
+#         for i in range(1,len(height)):
+#             left[i]=max(left[i-1], height[i])
+        
+#         for i in range(len(height)-2,-1,-1):
+#             right[i]=max(right[i+1], height[i])
+#         print(left, right)
+        l,r=0, len(height)-1
+        area=0
         while l<r:
-            curr_area=max(curr_area,min(h[l],h[r])*(r-l))
-            if h[l]<h[r]:
+            area=max(area, min(height[l], height[r])*(r-l))
+            if height[l]<height[r]:
                 l+=1
             else:
                 r-=1
-        return curr_area
+        return area
+        
