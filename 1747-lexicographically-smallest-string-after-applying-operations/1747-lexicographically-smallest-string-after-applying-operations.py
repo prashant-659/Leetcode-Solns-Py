5b04146caf0566ad0424ff6d5a614b7c35c0d200
Prashant <97583940+prashant-659@users.noreply.github.com>
2025-10-19 13:56:15 +0530

    Time: 519 ms (82.73%), Space: 30.7 MB (26.62%) - LeetHub

--- 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.py
+++ 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.py
@@ -1,28 +1,29 @@
 class Solution:
     def findLexSmallestString(self, s: str, a: int, b: int) -> str:
-        visit=set()
-        res=[s]
-        def dfs(s,a,b):
-            #base case 
-            if s in visit:
+        n = len(s)
+        # this map holds the new value of every digit after addition by a % 10
+        incremented = {str(n):str((n+a)%10) for n in range(10)}
+
+        # function applying the addition operation
+        def addOp(s):
+            res = ""
+            for i in range(n):
+                res += s[i] if i % 2 == 0 else incremented[s[i]]
+            return res
+
+        # function applying the rotation operation
+        def rotOp(s):
+            return s[n-b:] + s[:n-b]
+
+
+        seen = set()
+        def dfs(s):
+            if s in seen:
                 return
-            visit.add(s)
-            ans=""
-            for i in range(len(s)):
-                if i%2!=0:
-                    ans+=str((int(s[i])+a)%10)
-                else:
-                    ans+=s[i]
-            if ans<res[0]:
-                res[0]=ans
-            dfs(ans,a,b)
-            s=s[b:]+s[:b]
-            if s<res[0]:
-                res[0]=s
-            dfs(s,a,b)
+            seen.add(s)
+            dfs(addOp(s))
+            dfs(rotOp(s))
             return
-        dfs(s,a,b)
-        return res[0]
 
-
-
+        dfs(s)
+        return min(seen)
